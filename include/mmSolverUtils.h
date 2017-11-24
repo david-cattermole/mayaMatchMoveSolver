150a37a2abe6543d4f510158f32e5dd994ccb07c
david-cattermole <cattermole91@gmail.com>
2017-11-24 18:50:25 +1030

    Support for multi-frame solving. Added MComputation support so the user can cancel a solve before it's finished - we do a hack to get levmar to exit. Added benchmarks for parameter setting and error measurement. List of errors is hacky returned to the calling function. Using a few index lookup variables to remap values for frame list.

--- include/mmSolverUtils.h
+++ include/mmSolverUtils.h
@@ -20,7 +20,6 @@
 
 // Utils
 #include <utilities/debugUtils.h>
-#include <utilities/cgCameraUtils.h>
 
 // Maya
 #include <maya/MPoint.h>
@@ -35,6 +34,7 @@
 #include <maya/MMatrix.h>
 #include <maya/MFloatMatrix.h>
 #include <maya/MFnCamera.h>
+#include <maya/MComputation.h>
 
 // GL Math
 #include <glm/glm.hpp>
@@ -73,10 +73,20 @@ struct SolverData {
     MarkerPtrList markerList;
     BundlePtrList bundleList;
     AttrPtrList attrList;
+    MTimeArray frameList;  // Times to solve
+
+    // Relational mapping indexes.
+    std::vector< std::pair<int, int> > paramToAttrList;
+    std::vector< std::pair<int, int> > errorToMarkerList;
+    std::vector<double> errorList;
+    int iterNum;
+    debug::TimestampBenchmark *errorBench;
+    debug::TimestampBenchmark *paramBench;
 
     // Storing changes for undo/redo.
     MDGModifier      *dgmod;
-    MAnimCurveChange *animChange;
+    MAnimCurveChange *curveChange;
+    MComputation *computation;
     bool verbose;
 };
 
@@ -84,88 +94,85 @@ struct SolverData {
 // Function run by lev-mar algorithm to test the input parameters, p, and compute the output errors, x.
 inline
 void solveFunc(double *p, double *x, int m, int n, void *data) {
-    register int i, j;
-    SolverData *udata = static_cast<SolverData*>(data);
+    register int i = 0;
+    SolverData *ud = static_cast<SolverData*>(data);
+    ud->computation->setProgress(ud->iterNum);
+//    INFO("Solve #" << ++ud->iterNum);
 
-    CameraPtr cam = udata->cameraList[0];
-    MMatrix resultMat = cam->getWorldProjMatrix();
+    if (ud->computation->isInterruptRequested()){
+        WRN("User wants to cancel the solve!");
+        // This is an ugly hack to force levmar to stop computing. We give
+        // an infinite value which causes levmar detect it and quit the loop.
+        for (i=0; i<n; ++i){
+            x[i] = INFINITY;
+        }
+        return;
+    }
 
+    ud->paramBench->start();
+    MStatus status;
+    MTime time;
+    CameraPtr cam = ud->cameraList[0];
+    MMatrix resultMat;
+    status = cam->getWorldProjMatrix(resultMat);
+
+    MTime currentFrame = MAnimControl::currentTime();
     for (i = 0; i < m; ++i) {
-        MPlug plug = udata->attrList[i]->getPlug();
-//        INFO("set i=" << i << " " << plug.name() << " p=" << p[i]);
-        udata->dgmod->newPlugValueDouble(plug, p[i]);
+        std::pair<int, int> attrPair = ud->paramToAttrList[i];
+        AttrPtr attr = ud->attrList[attrPair.first];
+
+        // Get frame time
+        MTime frame = currentFrame;
+        if (attrPair.second != -1){
+            frame = ud->frameList[attrPair.second];
+        }
+
+        attr->setValue(p[i], frame, *ud->dgmod, *ud->curveChange);
     }
-    udata->dgmod->doIt();
-
-    // Force evaluate bundles
-    // TODO: Do Camera's also need to be updated here?
-    for (i = 0; i < (n / 3); ++i) {
-        MarkerPtr mkr = udata->markerList[i];
-        glm::vec3 mkr_pos;
-        mkr->getPos(mkr_pos);
-
-        BundlePtr bnd = mkr->getBundle();
-        glm::vec3 bnd_pos;
-        bnd->getPos(bnd_pos);
-    }
-
-    // NOTE: In order to handle undo/redo, we must use a 'MDGModifier' class. Such a class does not support time
-    // changes or setting plug values at specific times; this doesn't matter. Any plug value set should only be a
-    // 'static' attribute value in any case. Any plug value get can be achieved with a MDGContext class in the plug.
-    // Plug values at different times must be animCurves and therefore we can use MFnAnimCurve for value setting and
-    // MAnimCurveChange for undo/redo. Otherwise the only need to change times will be for evaluation at a specific
-    // time.
+    ud->dgmod->doIt();
+    ud->paramBench->stop();
 
     // Calculate
+    ud->errorBench->start();
     MPoint mkr_mpos;
     MPoint bnd_mpos;
     for (i = 0; i < (n / 3); ++i) {
-        MarkerPtr mkr = udata->markerList[i];
-        mkr->getPos(mkr_mpos);
-//        INFO("mkr name=" << mkr->getNodeName()
-//                         << " x=" << mkr_mpos.x
-//                         << " y=" << mkr_mpos.y
-//                         << " z=" << mkr_mpos.z
-//                         << " w=" << mkr_mpos.w
-//        );
+        std::pair<int, int> markerPair = ud->errorToMarkerList[i];
+        MarkerPtr marker = ud->markerList[markerPair.first];
+        MTime frame = ud->frameList[markerPair.second];
+
+        // TODO: Test to see if the first dummy call at a different frame is needed.
+        status = marker->getPos(mkr_mpos, frame+1);
+        CHECK_MSTATUS(status);
+        status = marker->getPos(mkr_mpos, frame);
+        CHECK_MSTATUS(status);
         mkr_mpos = mkr_mpos * resultMat;
         mkr_mpos.cartesianize();
-
-//        INFO("mkr name=" << mkr->getNodeName()
-//                         << " x=" << mkr_mpos.x
-//                         << " y=" << mkr_mpos.y
-//                         << " z=" << mkr_mpos.z
-//                         << " w=" << mkr_mpos.w
-//        );
         glm::vec2 mkr_pos2d(mkr_mpos.x, mkr_mpos.y);
 
-        BundlePtr bnd = mkr->getBundle();
-        bnd->getPos(bnd_mpos);
-//        INFO("bnd name=" << bnd->getNodeName()
-//                         << " x=" << bnd_mpos.x
-//                         << " y=" << bnd_mpos.y
-//                         << " z=" << bnd_mpos.z
-//                         << " w=" << bnd_mpos.w
-//        );
+        BundlePtr bnd = marker->getBundle();
+        status = bnd->getPos(bnd_mpos, frame-1);
+        CHECK_MSTATUS(status);
+        status = bnd->getPos(bnd_mpos, frame);
+        CHECK_MSTATUS(status);
         bnd_mpos = bnd_mpos * resultMat;
         bnd_mpos.cartesianize();
-//        INFO("bnd name=" << bnd->getNodeName()
-//                         << " x=" << bnd_mpos.x
-//                         << " y=" << bnd_mpos.y
-//                         << " z=" << bnd_mpos.z
-//                         << " w=" << bnd_mpos.w
-//        );
         glm::vec2 bnd_pos2d(bnd_mpos.x, bnd_mpos.y);
 
         // NOTE: Interestingly, using an x, y and distance error measurement seems to allow at least some scenes to converge much faster; ~20 iterations compared to ~160 iterations.
-        double dx = fabs(mkr_pos2d.x - bnd_pos2d.x);
-        double dy = fabs(mkr_pos2d.y - bnd_pos2d.y);
+        double dx = fabs(mkr_mpos.x - bnd_mpos.x);
+        double dy = fabs(mkr_mpos.y - bnd_mpos.y);
         double d = fabs(glm::distance(bnd_pos2d, mkr_pos2d));
-//        INFO("err " << i << " : dx=" << dx << " dy=" << dy << " d=" << d);
-        x[i+0] = 0.5 * (dx * dx);  // X error
-        x[i+1] = 0.5 * (dy * dy);  // Y error
-        x[i+2] = 0.5 * (d * d);    // Distance error
+        x[(i*3)+0] = dx;  // X error
+        x[(i*3)+1] = dy;  // Y error
+        x[(i*3)+2] = d;   // Distance error
+
+        ud->errorList[(i*3)+0] = dx;
+        ud->errorList[(i*3)+1] = dy;
+        ud->errorList[(i*3)+2] = dy;
     }
+    ud->errorBench->stop();
+    return;
 }
 
 
@@ -175,57 +182,138 @@ bool solve(int iterMax,
            std::vector< std::shared_ptr<Marker> > markerList,
            std::vector< std::shared_ptr<Bundle> > bundleList,
            std::vector< std::shared_ptr<Attr> > attrList,
+           MTimeArray frameList,
            MDGModifier &dgmod,
-           // MAnimCurveChange &animChange,
+           MAnimCurveChange &curveChange,
+           MComputation &computation,
            double &outError) {
     register int i, j;
+    MStatus status;
     int ret;
 
     // Number of unknown parameters.
-    const int m = (const int) attrList.size();
+    int m = 0;
+
+    // Count up number of unknown parameters
+    std::vector< std::pair<int,int> > paramToAttrList;
+    i = 0;
+    j = 0;
+    for (AttrPtrListIt ait = attrList.begin(); ait != attrList.end(); ++ait){
+        INFO("ait=" << i << " m=" << m);
+        AttrPtr attr = *ait;
+        if (attr->getDynamic()) {
+            m += frameList.length();
+            for (j=0; j < (int) frameList.length(); ++j){
+                // first index is into 'attrList'
+                // second index is into 'frameList'
+                std::pair<int, int> attrPair(i, j);
+                paramToAttrList.push_back(attrPair);
+            }
+        } else {
+            ++m;
+            // first index is into 'attrList'
+            // second index is into 'frameList', '-1' means a static value.
+            std::pair<int, int> attrPair(i, -1);
+            paramToAttrList.push_back(attrPair);
+        }
+        i++;
+    }
     double params[m];
 
     // Number of measurement errors. (Must be less than or equal to number of unknown parameters).
-    int n = (int) (markerList.size() * 3);  // TODO: Revise this when more cameras are added into the solve.
-    INFO("m=" << m);
-    INFO("n=" << n);
+    int n = 0;
+
+    // Count up number of errors
+    // For each marker on each frame that it is valid, we add 3 errors.
+    std::vector< std::pair<int,int> > errorToMarkerList;
+    i = 0;
+    j = 0;
+    for (MarkerPtrListIt mit = markerList.begin(); mit != markerList.end(); ++mit){
+        MarkerPtr marker = *mit;
+        for (j = 0; j < (int) frameList.length(); ++j){
+            MTime frame = frameList[j];
+            bool valid;
+            status = marker->getValid(valid, frame);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            if (valid){
+                std::pair<int, int> markerPair(i, j);
+                errorToMarkerList.push_back(markerPair);
+                n += 3;
+            }
+        }
+        i++;
+    }
+
+    INFO("params m=" << m);
+    INFO("errors n=" << n);
     assert(m <= n);
+    std::vector<double> errorList;
+    errorList.resize((unsigned long) n);
+    for (i=0; i<n; ++i) {
+        errorList[i] = 0.0;
+    }
 
     // Standard Lev-Mar arguments.
     double opts[LM_OPTS_SZ];
     double info[LM_INFO_SZ];
 
     // Options
-    double nudgeFactor = 1.0; // 1E+6;
+    // TODO: Test for the best nudge factor. Should nudgeFactor be a function input variable?
+    double nudgeFactor = 10.0;
     opts[0] = LM_INIT_MU * nudgeFactor;
     opts[1] = 1E-15;
     opts[2] = 1E-15;
     opts[3] = 1E-20;
-    opts[4] = -LM_DIFF_DELTA * nudgeFactor;
+    opts[4] = -LM_DIFF_DELTA * (nudgeFactor * 0.1);
+
+    // Debug timers
+    debug::TimestampBenchmark errorBench = debug::TimestampBenchmark();
+    debug::TimestampBenchmark paramBench = debug::TimestampBenchmark();
 
     struct SolverData userData;
     userData.cameraList = cameraList;
     userData.markerList = markerList;
     userData.bundleList = bundleList;
     userData.attrList = attrList;
+    userData.frameList = frameList;
+    userData.paramToAttrList = paramToAttrList;
+    userData.errorToMarkerList = errorToMarkerList;
+    userData.errorList = errorList;
+    userData.iterNum = 0;
+    userData.errorBench = &errorBench;
+    userData.paramBench = &paramBench;
     userData.dgmod = &dgmod;
-    // userData.animChange = &animChange;
+    userData.curveChange = &curveChange;
+    userData.computation = &computation;
 
     // Set Initial parameters
     INFO("Set Initial parameters");
+    MTime currentFrame = MAnimControl::currentTime();
     i = 0;
-    for (AttrPtrListIt ait = attrList.begin(); ait != attrList.end(); ++ait, ++i){
-        AttrPtr attr = *ait;
-        MPlug plug = attr->getPlug();
-        params[i] = plug.asDouble();
+    for (i=0; i<m; ++i){
+        std::pair<int, int> attrPair = paramToAttrList[i];
+        AttrPtr attr = attrList[attrPair.first];
+
+        // Get frame time
+        MTime frame = currentFrame;
+        if (attrPair.second != -1){
+            frame = frameList[attrPair.second];
+        }
+
+        double value;
+        status = attr->getValue(value, frame);
+        CHECK_MSTATUS_AND_RETURN(status, false);
+
+        params[i] = value;
     }
 
-    // Initial Parameters
-    INFO("Initial Parameters: ");
-    for (i = 0; i < m; ++i) {
-        INFO("-> " << params[i]);
-    }
-    INFO("");
+    // // Initial Parameters
+    // INFO("Initial Parameters: ");
+    // for (i = 0; i < m; ++i) {
+    //     INFO("-> " << params[i]);
+    // }
+    // INFO("");
 
     // Allocate a memory block for both 'work' and 'covar', so that
     // the block is close together in physical memory.
@@ -238,6 +326,10 @@ bool solve(int iterMax,
     covar = work + LM_DIF_WORKSZ(m, n);
 
     // no Jacobian, caller allocates work memory, covariance estimated
+    INFO("Solving...");
+    INFO("Maximum Iterations=" << iterMax);
+    computation.setProgressRange(0, iterMax);
+    computation.beginComputation();
     ret = dlevmar_dif(
 
             // Function to call (input only)
@@ -318,15 +410,7 @@ bool solve(int iterMax,
             // pointer to possibly needed additional data, passed uninterpreted to func.
             // Set to NULL if not needed
             (void *) &userData);
-
-//    INFO("Covariance of the fit:");
-//    for (i = 0; i < m; ++i) {
-//        for (j = 0; j < m; ++j) {
-//            INFO(covar[i * m + j]);
-//        }
-//        INFO("");
-//    }
-//    INFO("");
+    computation.endComputation();
 
     free(work);
 
@@ -334,6 +418,8 @@ bool solve(int iterMax,
     INFO("Levenberg-Marquardt returned " << ret << " in " << (int) info[5]
                                          << " iterations");
 
+    // Build in some logic as to what happens when the solver finishes due to various problems.
+    // We could restart the solve with an increased "mu" value if we get reason 4 or 5. We also need to cap the number of restarts, so we don't restart recursively.
     int reasonNum = (int) info[6];
     INFO("Reason: " << reasons[reasonNum]);
     INFO("Reason number: " << info[6]);
@@ -345,10 +431,18 @@ bool solve(int iterMax,
     }
     INFO("");
 
+    // Compute the average error based on the error values the solve function last computed.
+    // TODO: Create a list of frames and produce an error per-frame. This information will eventually be given to the user to diagnose problems.
+    double avgError = 0;
+    for (i = 0; i < n; ++i) {
+        avgError += userData.errorList[i];
+    }
+    avgError /= (double) n;
+
     INFO(std::endl << std::endl << "Solve Information:");
     INFO("Initial Error: " << info[0]);
-
-    INFO("Overall Error: " << info[1]);
+    INFO("Final Error: " << info[1]);
+    INFO("Average Error: " << avgError);
     INFO("J^T Error: " << info[2]);
     INFO("Dp Error: " << info[3]);
     INFO("Max Error: " << info[4]);
@@ -358,6 +452,9 @@ bool solve(int iterMax,
     INFO("Function Evaluations: " << info[7]);
     INFO("Jacobian Evaluations: " << info[8]);
     INFO("Attempts for reducing error: " << info[9]);
+
+    paramBench.printInSec("Param Time:", 1);
+    errorBench.printInSec("Error Time:", 1);
 
     outError = info[1];
 

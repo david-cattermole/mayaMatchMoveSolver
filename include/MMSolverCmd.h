ee9deb4cb581b45df3c65ed500c3ce330b8dea6e
david-cattermole <cattermole91@gmail.com>
2017-11-29 17:43:12 +1030

    Changed plugin class name to match UpperCamelCase naming convention. Changed #define names for plugin flags to adhere to naming convention. Removed start and end frame from command flags, added a frame list (provides better flexibility). Added error thresholds to command flags, allows user to try new error values. Added flag for solver type to the command flags; for levmar and splm. Added checks that node names given to the command are valid and are of the node type expected. Removed weight as an argument; this will be defined by an attribute.

---
+++ include/MMSolverCmd.h
@@ -0,0 +1,140 @@
+/*
+ * Header for mmSolver Maya command.
+ */
+
+#ifndef MAYA_MM_SOLVER_CMD_H
+#define MAYA_MM_SOLVER_CMD_H
+
+
+// STL
+#include <cmath>
+#include <vector>
+
+// Maya
+#include <maya/MGlobal.h>
+#include <maya/MIOStream.h>
+
+#include <maya/MPxCommand.h>
+#include <maya/MArgList.h>
+#include <maya/MArgDatabase.h>
+#include <maya/MSyntax.h>
+
+#include <maya/MSelectionList.h>
+#include <maya/MDagPath.h>
+#include <maya/MDGModifier.h>
+#include <maya/MDagModifier.h>
+#include <maya/MFnDagNode.h>
+
+#include <maya/MPlug.h>
+#include <maya/MPlugArray.h>
+#include <maya/MAnimCurveChange.h>
+#include <maya/MComputation.h>
+
+#include <maya/MPoint.h>
+#include <maya/MVector.h>
+#include <maya/MMatrix.h>
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
+#include <maya/MDoubleArray.h>
+
+#include <Camera.h>
+#include <Marker.h>
+#include <Bundle.h>
+#include <Attr.h>
+
+// Command arguments and command name
+#define CAMERA_FLAG       "-c"
+#define CAMERA_FLAG_LONG  "-camera"
+
+#define MARKER_FLAG       "-m"
+#define MARKER_FLAG_LONG  "-marker"
+
+#define ATTR_FLAG       "-a"
+#define ATTR_FLAG_LONG  "-attr"
+
+#define FRAME_FLAG       "-f"
+#define FRAME_FLAG_LONG  "-frame"
+
+#define SOLVER_TYPE_FLAG           "-st"
+#define SOLVER_TYPE_FLAG_LONG      "-solverType"
+#define SOLVER_TYPE_DEFAULT_VALUE  0  // levmar
+
+#define ITERATIONS_FLAG           "-it"
+#define ITERATIONS_FLAG_LONG      "-iterations"
+#define ITERATIONS_DEFAULT_VALUE  1000
+
+#define TAU_FLAG           "-t"
+#define TAU_FLAG_LONG      "-tauFactor"
+#define TAU_DEFAULT_VALUE  1E-03
+
+#define EPSILON1_FLAG           "-e1"
+#define EPSILON1_FLAG_LONG      "-epsilon1"
+#define EPSILON1_DEFAULT_VALUE  1E-12
+
+#define EPSILON2_FLAG           "-e2"
+#define EPSILON2_FLAG_LONG      "-epsilon2"
+#define EPSILON2_DEFAULT_VALUE  1E-12
+
+#define EPSILON3_FLAG           "-e3"
+#define EPSILON3_FLAG_LONG      "-epsilon3"
+#define EPSILON3_DEFAULT_VALUE  1E-12
+
+#define DELTA_FLAG           "-dt"
+#define DELTA_FLAG_LONG      "-delta"
+#define DELTA_DEFAULT_VALUE  -1E-03  // -1E-06
+
+#define VERBOSE_FLAG           "-v"
+#define VERBOSE_FLAG_LONG      "-verbose"
+#define VERBOSE_DEFAULT_VALUE  false
+
+#define COMMAND_NAME    "mmSolver"
+#define COMMAND_VERSION "1.0"
+
+
+class MMSolverCmd : public MPxCommand {
+public:
+
+    MMSolverCmd() {};
+
+    virtual ~MMSolverCmd();
+
+    virtual bool hasSyntax() const;
+    static MSyntax newSyntax();
+
+    virtual MStatus doIt(const MArgList &args);
+
+    virtual bool isUndoable() const;
+
+    virtual MStatus undoIt();
+
+    virtual MStatus redoIt();
+
+    static void *creator();
+
+private:
+    MStatus parseArgs( const MArgList& args );
+
+    // Solver Information.
+    unsigned int m_iterations;
+    double m_tau;       // Scale factor for initialTransform mu
+    double m_epsilon1;  // Stopping threshold for ||J^T e||_inf
+    double m_epsilon2;  // Stopping threshold for ||Dp||_2
+    double m_epsilon3;  // Stopping threshold for ||e||_2
+    double m_delta;     // Step used in difference approximation to the Jacobian
+    int m_solverType;   // Solver type to use; 0=levmar, 1=splm (sparse levmar).
+    bool m_verbose;
+
+    // Objects
+    CameraPtrList m_cameraList;
+    MarkerPtrList m_markerList;
+    BundlePtrList m_bundleList;
+    AttrPtrList   m_attrList;
+    MTimeArray m_frameList;
+
+    // Undo/Redo
+    MDGModifier m_dgmod;
+    MAnimCurveChange m_curveChange;
+    MComputation m_computation;
+};
+
+#endif // MAYA_MM_SOLVER_CMD_H
---
+++ src/MMSolverCmd.cpp
@@ -0,0 +1,428 @@
+/*
+ * Command for running mmSolver.
+ */
+
+
+//
+#include <MMSolverCmd.h>
+#include <mmSolverUtils.h>
+#include <mayaUtils.h>
+
+// STL
+#include <cmath>
+
+// Utils
+#include <utilities/debugUtils.h>
+
+// Maya
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
+#include <maya/MObject.h>
+#include <maya/MPlug.h>
+#include <maya/MFnDependencyNode.h>
+
+// Internal Objects
+#include <Marker.h>
+#include <Bundle.h>
+#include <Camera.h>
+
+
+MMSolverCmd::~MMSolverCmd() {}
+
+void *MMSolverCmd::creator() {
+    return new MMSolverCmd();
+}
+
+/*
+ * Tell Maya we have a syntax function.
+ */
+bool MMSolverCmd::hasSyntax() const {
+    return true;
+}
+
+bool MMSolverCmd::isUndoable() const {
+    return true;
+}
+
+/*
+ * Add flags to the command syntax
+ */
+MSyntax MMSolverCmd::newSyntax() {
+    MSyntax syntax;
+    syntax.enableQuery(false);
+    syntax.enableEdit(false);
+
+    // Flags
+    syntax.addFlag(CAMERA_FLAG, CAMERA_FLAG_LONG, MSyntax::kString, MSyntax::kString);
+    syntax.addFlag(MARKER_FLAG, MARKER_FLAG_LONG, MSyntax::kString, MSyntax::kString, MSyntax::kString);
+    syntax.addFlag(ATTR_FLAG, ATTR_FLAG_LONG, MSyntax::kString, MSyntax::kBoolean);
+    syntax.addFlag(FRAME_FLAG, FRAME_FLAG_LONG, MSyntax::kLong);
+    syntax.addFlag(TAU_FLAG, TAU_FLAG_LONG, MSyntax::kDouble);
+    syntax.addFlag(EPSILON1_FLAG, EPSILON1_FLAG_LONG, MSyntax::kDouble);
+    syntax.addFlag(EPSILON2_FLAG, EPSILON2_FLAG_LONG, MSyntax::kDouble);
+    syntax.addFlag(EPSILON3_FLAG, EPSILON3_FLAG_LONG, MSyntax::kDouble);
+    syntax.addFlag(DELTA_FLAG, DELTA_FLAG_LONG, MSyntax::kDouble);
+    syntax.addFlag(SOLVER_TYPE_FLAG, SOLVER_TYPE_FLAG_LONG, MSyntax::kUnsigned);
+    syntax.addFlag(ITERATIONS_FLAG, ITERATIONS_FLAG_LONG, MSyntax::kUnsigned);
+    syntax.addFlag(VERBOSE_FLAG, VERBOSE_FLAG_LONG, MSyntax::kBoolean);
+
+    // We can use marker and attr flags more than once.
+    syntax.makeFlagMultiUse(CAMERA_FLAG);
+    syntax.makeFlagMultiUse(MARKER_FLAG);
+    syntax.makeFlagMultiUse(ATTR_FLAG);
+    syntax.makeFlagMultiUse(FRAME_FLAG);
+
+    return syntax;
+}
+
+/*
+ * Parse command line arguments
+ */
+MStatus MMSolverCmd::parseArgs(const MArgList &args) {
+    MStatus status = MStatus::kSuccess;
+
+    MArgDatabase argData(syntax(), args, &status);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    m_cameraList.clear();
+    m_markerList.clear();
+    m_bundleList.clear();
+
+    // Get 'Camera'
+    MString cameraTransform = "";
+    MString cameraShape = "";
+    unsigned int cameraNum = argData.numberOfFlagUses(CAMERA_FLAG);
+    for (unsigned int i = 0; i < cameraNum; ++i) {
+
+        MArgList cameraArgs;
+        status = argData.getFlagArgumentList(CAMERA_FLAG, i, cameraArgs);
+        if (status == MStatus::kSuccess) {
+            if (cameraArgs.length() != 2) {
+                ERR("Camera argument list must have 2 arguments; \"cameraTransform\", \"cameraShape\".");
+                continue;
+            }
+
+            cameraTransform = cameraArgs.asString(0, &status);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+            nodeExistsAndIsType(cameraTransform, MFn::Type::kTransform);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            cameraShape = cameraArgs.asString(1, &status);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+            nodeExistsAndIsType(cameraShape, MFn::Type::kCamera);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            CameraPtr camera = CameraPtr(new Camera());
+            camera->setTransformNodeName(cameraTransform);
+            camera->setShapeNodeName(cameraShape);
+            m_cameraList.push_back(camera);
+
+            INFO("Camera = " << i);
+            INFO("  Transform = " << camera->getTransformNodeName());
+            INFO("  Shape = " << camera->getShapeNodeName());
+        }
+    }
+
+    // Get 'Markers'
+    MString markerName = "";
+    MString cameraName = "";
+    MString bundleName = "";
+    unsigned int markerNum = argData.numberOfFlagUses(MARKER_FLAG);
+    for (unsigned int i = 0; i < markerNum; ++i) {
+        MArgList markerArgs;
+        status = argData.getFlagArgumentList(MARKER_FLAG, i, markerArgs);
+        if (status == MStatus::kSuccess) {
+            if (markerArgs.length() != 3) {
+                ERR("Marker argument list must have 3 arguments; \"marker\", \"cameraShape\",  \"bundle\".");
+                continue;
+            }
+
+            markerName = markerArgs.asString(0, &status);
+            INFO("markerName=" << markerName);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+            status = nodeExistsAndIsType(markerName, MFn::Type::kTransform);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            cameraName = markerArgs.asString(1, &status);
+            INFO("cameraName=" << cameraName);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+            status = nodeExistsAndIsType(cameraName, MFn::Type::kCamera);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            bundleName = markerArgs.asString(2, &status);
+            INFO("bundleName=" << bundleName);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+            status = nodeExistsAndIsType(bundleName, MFn::Type::kTransform);
+            CHECK_MSTATUS_AND_RETURN_IT(status);
+
+            // Camera
+            CameraPtr camera = CameraPtr(new Camera());
+            for (unsigned int j = 0; j < m_cameraList.size(); ++j) {
+                if (m_cameraList[j]->getShapeNodeName() == cameraName) {
+                    camera = m_cameraList[j];
+                    break;
+                }
+            }
+            if (camera->getShapeNodeName() == "") {
+                ERR("Camera shape name was not given with marker. "
+                            << "marker=" << markerName << " "
+                            << "camera=" << cameraName << " "
+                            << "bundle=" << bundleName);
+            }
+
+            // Bundle
+            BundlePtr bundle = BundlePtr(new Bundle());
+            for (unsigned int j = 0; j < m_bundleList.size(); ++j) {
+                if (m_bundleList[j]->getNodeName() == bundleName) {
+                    bundle = m_bundleList[j];
+                    break;
+                }
+            }
+            if (bundle->getNodeName() == "") {
+                bundle->setNodeName(bundleName);
+            }
+
+            // Marker
+            for (unsigned int j = 0; j < m_markerList.size(); ++j) {
+                if (m_markerList[j]->getNodeName() == markerName) {
+                    ERR("Marker name cannot be specified more than once. markerName=" << markerName);
+                }
+            }
+            MarkerPtr marker = MarkerPtr(new Marker());
+            marker->setNodeName(markerName);
+            marker->setBundle(bundle);
+            marker->setCamera(camera);
+
+            m_markerList.push_back(marker);
+            m_bundleList.push_back(bundle);
+
+            INFO("Marker = " << i);
+            INFO("  Camera = " << cameraName << " : " << camera->getTransformNodeName() << " : "
+                               << camera->getShapeNodeName());
+            INFO("  Marker = " << markerName << " : " << marker->getNodeName());
+            INFO("  Bundle = " << bundleName << " : " << bundle->getNodeName());
+        }
+    }
+
+    // Get 'Attributes'
+    m_attrList.clear();
+    unsigned int attrsNum = argData.numberOfFlagUses(ATTR_FLAG);
+    for (unsigned int i = 0; i < attrsNum; ++i) {
+        MArgList attrArgs;
+        status = argData.getFlagArgumentList(ATTR_FLAG, i, attrArgs);
+        if (status == MStatus::kSuccess) {
+            if (attrArgs.length() != 2) {
+                ERR("Attribute argument list must have 2 argument; \"node.attribute\", \"dynamic\".");
+                continue;
+            }
+
+            AttrPtr attr = AttrPtr(new Attr());
+            MString nodeAttrName = attrArgs.asString(0);
+            bool dyn = attrArgs.asBool(1);
+            attr->setName(nodeAttrName);
+            attr->setDynamic(dyn);
+            m_attrList.push_back(attr);
+
+            MPlug attrPlug = attr->getPlug();
+            INFO("Attr = " << i << " : "
+                           << attr->getName() << " : "
+                           << attr->getNodeName() << " : "
+                           << attr->getDynamic() << " : "
+                           << attrPlug.name());
+        }
+    }
+
+    // Get 'Frames'
+    m_frameList.clear();
+    MTime::Unit unit = MTime::uiUnit();
+    unsigned int framesNum = argData.numberOfFlagUses(FRAME_FLAG);
+    for (unsigned int i = 0; i < framesNum; ++i) {
+        MArgList frameArgs;
+        status = argData.getFlagArgumentList(FRAME_FLAG, i, frameArgs);
+        if (status == MStatus::kSuccess) {
+            if (frameArgs.length() != 1) {
+                ERR("Attribute argument list must have 1 argument; \"frame\".");
+                continue;
+            }
+            int value = frameArgs.asInt(0, &status);
+            CHECK_MSTATUS(status);
+
+            MTime frame = MTime((double) value, unit);
+            m_frameList.append(frame);
+            INFO("Frame = " << i << " : " << frame << " : " << value);
+        }
+    }
+
+    // Make sure we have a frame list.
+    if (m_frameList.length() == 0) {
+        status = MS::kFailure;
+        status.perror("Frame List length is 0, must have a frame to solve.");
+        return status;
+    }
+
+    // Get 'Iterations'
+    m_iterations = ITERATIONS_DEFAULT_VALUE;
+    if (argData.isFlagSet(ITERATIONS_FLAG)) {
+        status = argData.getFlagArgument(ITERATIONS_FLAG, 0, m_iterations);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_iterations=" << m_iterations);
+
+    // Get 'Tau'
+    m_tau = TAU_DEFAULT_VALUE;
+    if (argData.isFlagSet(TAU_FLAG)) {
+        status = argData.getFlagArgument(TAU_FLAG, 0, m_tau);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_tau=" << m_tau);
+
+    // Get 'Epsilon1'
+    m_epsilon1 = EPSILON1_DEFAULT_VALUE;
+    if (argData.isFlagSet(EPSILON1_FLAG)) {
+        status = argData.getFlagArgument(EPSILON1_FLAG, 0, m_epsilon1);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_epsilon1=" << m_epsilon1);
+
+    // Get 'Epsilon2'
+    m_epsilon2 = EPSILON2_DEFAULT_VALUE;
+    if (argData.isFlagSet(EPSILON2_FLAG)) {
+        status = argData.getFlagArgument(EPSILON2_FLAG, 0, m_epsilon2);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_epsilon2=" << m_epsilon2);
+
+    // Get 'Epsilon3'
+    m_epsilon3 = EPSILON3_DEFAULT_VALUE;
+    if (argData.isFlagSet(EPSILON3_FLAG)) {
+        status = argData.getFlagArgument(EPSILON3_FLAG, 0, m_epsilon3);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_epsilon3=" << m_epsilon3);
+    
+    // Get 'Delta'
+    m_delta = DELTA_DEFAULT_VALUE;
+    if (argData.isFlagSet(DELTA_FLAG)) {
+        status = argData.getFlagArgument(DELTA_FLAG, 0, m_delta);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_delta=" << m_delta);
+
+    // Get 'Solver Type'
+    m_solverType = SOLVER_TYPE_DEFAULT_VALUE;
+    if (argData.isFlagSet(SOLVER_TYPE_FLAG)) {
+        status = argData.getFlagArgument(SOLVER_TYPE_FLAG, 0, m_solverType);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_solverType=" << m_solverType);
+
+    // Get 'Verbose'
+    m_verbose = VERBOSE_DEFAULT_VALUE;
+    if (argData.isFlagSet(VERBOSE_FLAG)) {
+        status = argData.getFlagArgument(VERBOSE_FLAG, 0, m_verbose);
+        CHECK_MSTATUS(status);
+    }
+    INFO("m_verbose=" << m_verbose);
+
+    return status;
+}
+
+
+MStatus MMSolverCmd::doIt(const MArgList &args) {
+//
+//  Description:
+//    implements the MEL mmSolver command.
+//
+//  Arguments:
+//    argList - the argument list that was passes to the command from MEL
+//
+//  Return Value:
+//    MS::kSuccess - command succeeded
+//    MS::kFailure - command failed (returning this value will cause the
+//                     MEL script that is being run to terminate unless the
+//                     error is caught using a "catch" statement.
+//
+    MStatus status = MStatus::kSuccess;
+    INFO("MMSolverCmd::doIt()");
+
+    // Mouse cursor spinning...
+    MGlobal::executeCommand("waitCursor -state on;");
+
+    // Read all the flag arguments.
+    status = parseArgs(args);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+    assert(m_frameList.length() > 0);
+
+    // Don't store each individial edits, just store the combination of edits.
+    m_curveChange.setInteractive(true);
+
+    double outError = -1.0;
+    bool ret = solve(
+            m_iterations,
+            m_tau,
+            m_epsilon1,
+            m_epsilon2,
+            m_epsilon3,
+            m_delta,
+            m_solverType,
+            m_cameraList,
+            m_markerList,
+            m_bundleList,
+            m_attrList,
+            m_frameList,
+            m_dgmod,
+            m_curveChange,
+            m_computation,
+            outError
+    );
+    MMSolverCmd::setResult(outError);
+    if (ret == false) {
+        WRN("mmSolver: Solver returned false!");
+    }
+
+    // Mouse cursor back to normal.
+    MGlobal::executeCommand("waitCursor -state off;");
+    return status;
+}
+
+MStatus MMSolverCmd::redoIt() {
+//
+//  Description:
+//    Implements redo for the MEL mmSolver command.
+//
+//    This method is called when the user has undone a command of this type
+//    and then redoes it.  No arguments are passed in as all of the necessary
+//    information is cached by the doIt method.
+//
+//  Return Value:
+//    MS::kSuccess - command succeeded
+//    MS::kFailure - redoIt failed.  this is a serious problem that will
+//                     likely cause the undo queue to be purged
+//
+    MStatus status;
+    INFO("MMSolverCmd::redoIt()");
+    m_dgmod.doIt();
+    m_curveChange.redoIt();
+    return status;
+}
+
+MStatus MMSolverCmd::undoIt() {
+//
+//  Description:
+//    implements undo for the MEL mmSolver command.
+//
+//    This method is called to undo a previous command of this type.  The
+//    system should be returned to the exact state that it was it previous
+//    to this command being executed.  That includes the selection state.
+//
+//  Return Value:
+//    MS::kSuccess - command succeeded
+//    MS::kFailure - redoIt failed.  this is a serious problem that will
+//                     likely cause the undo queue to be purged
+//
+    MStatus status;
+    INFO("MMSolverCmd::undoIt()");
+    m_curveChange.undoIt();
+    m_dgmod.undoIt();
+    return status;
+}

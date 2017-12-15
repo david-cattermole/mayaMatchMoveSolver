24a22bb13f0f9a1942fd7235ab9be9249dfe9066
david-cattermole <cattermole91@gmail.com>
2017-12-15 17:29:05 +1030

    We can get a dict data structure from the frame so we can put it into a JSON format. Allow setting the frame number after class creation. Still need to have a frame number to create the class, but the user can use a dummy number anyway, then overwrite it with set_number or set_data.

--- python/mmSolver/_api/frame.py
+++ python/mmSolver/_api/frame.py
@@ -8,6 +8,7 @@ creating function callbacks for inclusion/exclusion of frames.
 import maya.cmds
 import maya.OpenMaya as OpenMaya
 import mmSolver._api.utils as api_utils
+import mmSolver._api.excep as excep
 
 
 class Frame(object):
@@ -18,9 +19,44 @@ class Frame(object):
         self._secondary = secondary
         return
 
+    def get_data(self):
+        return {
+            'number': self.get_number(),
+            'secondary': self.get_primary(),
+            'primary': self.get_secondary(),
+        }
+
+    def set_data(self, value):
+        assert isinstance(value, dict)
+
+        num = value.get('number')
+        pri = value.get('primary')
+        sec = value.get('secondary')
+        if num is None:
+            msg = 'Data is not valid, number value not valid; {0}'
+            msg = msg.format(value)
+            raise excep.NotValid, msg
+        if pri is None:
+            msg = 'Data is not valid, primary value not valid; {0}'
+            msg = msg.format(value)
+            raise excep.NotValid, msg
+        if sec is None:
+            msg = 'Data is not valid, secondary value not valid; {0}'
+            msg = msg.format(value)
+            raise excep.NotValid, msg
+
+        self.set_number(num)
+        self.set_primary(pri)
+        self.set_secondary(sec)
+
     def get_number(self):
         # TODO: Should we convert _number to an int or float before returning?
         return self._number
+
+    def set_number(self, value):
+        assert isinstance(value, (int, float))
+        self._number = value
+        return
 
     def get_primary(self):
         return self._primary

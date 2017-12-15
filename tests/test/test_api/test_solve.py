47a0805a835eaeec37d39630f88a511f21749b90
david-cattermole <cattermole91@gmail.com>
2017-12-15 17:24:23 +1030

    Use the API to create bundles and markers. Set and create Frame objects.

--- tests/test/test_api/test_solve.py
+++ tests/test/test_api/test_solve.py
@@ -20,7 +20,7 @@ import mmSolver.api as api
 import test.test_api.apiutils as test_api_utils
 
 
-@unittest.skip
+# @unittest.skip
 class TestSolve(test_api_utils.APITestCase):
 
     def test_init(self):
@@ -33,42 +33,38 @@ class TestSolve(test_api_utils.APITestCase):
         maya.cmds.setAttr(cam_tfm + '.tx', -1.0)
         maya.cmds.setAttr(cam_tfm + '.ty',  1.0)
         maya.cmds.setAttr(cam_tfm + '.tz', -5.0)
+        cam = api.Camera(shape=cam_shp)
 
         # Bundle
-        bundle_tfm = maya.cmds.createNode('transform',
-                                          name='bundle_tfm')
-        bundle_shp = maya.cmds.createNode('locator',
-                                          name='bundle_shp',
-                                          parent=bundle_tfm)
+        bnd = api.Bundle().create_node()
+        bundle_tfm = bnd.get_node()
         maya.cmds.setAttr(bundle_tfm + '.tx', 5.5)
         maya.cmds.setAttr(bundle_tfm + '.ty', 6.4)
         maya.cmds.setAttr(bundle_tfm + '.tz', -25.0)
-        bnd = api.Bundle(bundle_tfm)
+        assert api.get_object_type(bundle_tfm) == 'bundle'
 
         # Marker
-        marker_tfm = maya.cmds.createNode('transform',
-                                          name='marker_tfm',
-                                          parent=cam_tfm)
-        marker_shp = maya.cmds.createNode('locator',
-                                          name='marker_shp',
-                                          parent=marker_tfm)
-        maya.cmds.setAttr(marker_tfm + '.tx', -2.5)
-        maya.cmds.setAttr(marker_tfm + '.ty', 1.3)
-        maya.cmds.setAttr(marker_tfm + '.tz', -10)
-        mkr = api.Marker(marker_tfm)
-        mkr.set_bundle(bnd)
+        mkr = api.Marker().create_node(cam=cam, bnd=bnd)
+        marker_tfm = mkr.get_node()
+        assert api.get_object_type(marker_tfm) == 'marker'
+        maya.cmds.setAttr(marker_tfm + '.tx', 0.0)
+        maya.cmds.setAttr(marker_tfm + '.ty', 0.0)
 
         # Attributes
         attr_tx = api.Attribute(bundle_tfm + '.tx')
         attr_ty = api.Attribute(bundle_tfm + '.ty')
+
+        # Frames
+        frm_list = [
+            api.Frame(1, primary=True)
+        ]
 
         # Solver
         sol = api.Solver()
         sol.set_max_iterations(1000)
         sol.set_solver_type(api.SOLVER_TYPE_LEVMAR)
         sol.set_verbose(True)
-        sol.add_frame(1)
-        sol.set_frame_list([1])
+        sol.set_frame_list(frm_list)
 
         # Collection
         col = api.Collection()
@@ -79,13 +75,11 @@ class TestSolve(test_api_utils.APITestCase):
         col.add_attribute(attr_ty)
 
         # Run solver!
-        s = time.time()
         results = col.execute()
-        e = time.time()
-        print 'total time:', e - s
 
         # Ensure the values are correct
         for res in results:
+            success = res.get_success()
             err = res.get_error()
             assert self.approx_equal(err, 0.0, eps=0.001)
         assert self.approx_equal(maya.cmds.getAttr(bundle_tfm+'.tx'), -6.0)

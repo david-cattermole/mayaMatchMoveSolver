9412ca5f79e460032d22ce7db3e44d72e90af8da
david-cattermole <cattermole91@gmail.com>
2018-01-01 18:09:38 +1030

    Node Type IDs.

---
+++ include/nodeTypeIds.h
@@ -0,0 +1,14 @@
+/*
+ * This file lists the Node Type IDs used in the MM Solver project.
+ * All node types must listed here.
+ */
+
+#ifndef MM_SOLVER_NODE_TYPE_IDS_H
+#define MM_SOLVER_NODE_TYPE_IDS_H
+
+
+// #define MM_MARKER_SCALE_TYPE_ID 0x80028  // TODO: Change this!
+// #define MM_REPROJECTION_TYPE_ID 0x80029  // TODO: Change this!
+
+
+#endif // MM_SOLVER_NODE_TYPE_IDS_H

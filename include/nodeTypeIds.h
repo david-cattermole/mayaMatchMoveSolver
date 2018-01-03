c5c26369ae85c8291b64bcfe87de5b542fc659d1
david-cattermole <cattermole91@gmail.com>
2018-01-03 23:35:42 +1030

    Added node types.

--- include/nodeTypeIds.h
+++ include/nodeTypeIds.h
@@ -7,8 +7,8 @@
 #define MM_SOLVER_NODE_TYPE_IDS_H
 
 
-// #define MM_MARKER_SCALE_TYPE_ID 0x80028  // TODO: Change this!
-// #define MM_REPROJECTION_TYPE_ID 0x80029  // TODO: Change this!
+#define MM_MARKER_SCALE_TYPE_ID 0x80028  // TODO: Change this!
+#define MM_REPROJECTION_TYPE_ID 0x80029  // TODO: Change this!
 
 
 #endif // MM_SOLVER_NODE_TYPE_IDS_H

2d785f97eb3bc3ab2c0e8457601550d7546193d4
david-cattermole <cattermole91@gmail.com>
2018-01-03 23:35:04 +1030

    Add unit scale version constants.

--- include/utilities/numberUtils.h
+++ include/utilities/numberUtils.h
@@ -10,6 +10,14 @@
 #define RADIANS_TO_DEGREES 57.295779513082323 // 180.0/M_PI
 #define DEGREES_TO_RADIANS 0.017453292519943295 // M_PI/180.0
 
+
+// Conversion constants
+#define MM_TO_INCH 0.03937007874015748 // 0.03937
+#define MM_TO_CM 0.1
+#define INCH_TO_MM 25.4
+#define INCH_TO_CM 2.54
+
+
 // Memory Conversion
 #define BYTES_TO_KILOBYTES 1024 // int(pow(2, 10))
 #define BYTES_TO_MEGABYTES 1048576 // int(pow(2, 20))

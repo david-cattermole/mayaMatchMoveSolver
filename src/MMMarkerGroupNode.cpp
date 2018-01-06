a12a8026921b81d62187ada86c23139426a3a4e4
david-cattermole <cattermole91@gmail.com>
2018-01-06 18:34:46 +1030

    Marker Group transform node; a dummy transform with no overrides, the same as Maya's transform node.

---
+++ src/MMMarkerGroupNode.cpp
@@ -0,0 +1,43 @@
+/*
+ * A dummy transform node that acts exactly the same as Maya's normal
+ * transform node, but with a different icon.
+ */
+
+#include <MMMarkerGroupNode.h>
+
+#include <maya/MTypeId.h>
+#include <maya/MPxTransformationMatrix.h>
+
+#include <nodeTypeIds.h>
+
+#include <Camera.h>
+
+
+MTypeId MMMarkerGroupTransformNode::m_id(MM_MARKER_GROUP_TRANSFORM_TYPE_ID);
+
+MString MMMarkerGroupTransformNode::nodeName() {
+    return MString("mmMarkerGroup");
+}
+
+MMMarkerGroupTransformNode::MMMarkerGroupTransformNode() : MPxTransform() {}
+
+MMMarkerGroupTransformNode::MMMarkerGroupTransformNode(MPxTransformationMatrix *tm) : MPxTransform(tm) {}
+
+void MMMarkerGroupTransformNode::postConstructor() {
+    MPxTransform::postConstructor();
+    if (baseTransformationMatrix == NULL) {
+        baseTransformationMatrix = new MPxTransformationMatrix();
+    }
+}
+
+MMMarkerGroupTransformNode::~MMMarkerGroupTransformNode() = default;
+
+
+void *MMMarkerGroupTransformNode::creator() {
+    return (new MMMarkerGroupTransformNode());
+}
+
+
+MStatus MMMarkerGroupTransformNode::initialize() {
+    return MS::kSuccess;
+}

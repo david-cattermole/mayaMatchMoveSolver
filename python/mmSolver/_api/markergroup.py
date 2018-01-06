b4c51aae233a4857df21ab4464a0a97b9f368108
david-cattermole <cattermole91@gmail.com>
2018-01-06 20:16:06 +1030

    Add depth attribute to marker group.

--- python/mmSolver/_api/markergroup.py
+++ python/mmSolver/_api/markergroup.py
@@ -63,14 +63,23 @@ class MarkerGroup(object):
         cam_tfm = cam.get_transform_node()
         cam_shp = cam.get_shape_node()
 
-        mkr_grp = maya.cmds.createNode('mmMarkerGroupTransform', name=name, parent=cam_tfm)
+        mkr_grp = maya.cmds.createNode('mmMarkerGroupTransform',
+                                       name=name, parent=cam_tfm)
         mkr_scl = maya.cmds.createNode('mmMarkerScale')
         self.set_node(mkr_grp)
 
+        # Add attr and connect depth
+        maya.cmds.addAttr(mkr_grp, longName='depth', at='double', minValue=0.0,
+                          defaultValue=1.0)
+        maya.cmds.setAttr(mkr_grp + '.depth', keyable=True)
+        maya.cmds.connectAttr(mkr_grp + '.depth', mkr_scl + '.depth')
+
+        # Connect camera attributes
         maya.cmds.connectAttr(cam_shp + '.focalLength', mkr_scl + '.focalLength')
         maya.cmds.connectAttr(cam_shp + '.cameraAperture', mkr_scl + '.cameraAperture')
         maya.cmds.connectAttr(cam_shp + '.filmOffset', mkr_scl + '.filmOffset')
 
+        # Connect marker scale to marker group
         maya.cmds.connectAttr(mkr_scl + '.outScale', mkr_grp + '.scale')
         maya.cmds.connectAttr(mkr_scl + '.outTranslate', mkr_grp + '.translate')
 

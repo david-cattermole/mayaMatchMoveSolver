822e27a998be358f070aee26ce572e5460c2da83
david-cattermole <cattermole91@gmail.com>
2018-01-06 19:23:38 +1030

    Test functions for markergroup.

---
+++ tests/test/test_api/test_markergroup.py
@@ -0,0 +1,61 @@
+"""
+Test functions for markergroup module.
+"""
+
+import sys
+import os
+import unittest
+
+import maya.cmds
+
+import test.test_api.apiutils as test_api_utils
+import mmSolver._api.utils as api_utils
+import mmSolver._api.bundle as bundle
+import mmSolver._api.camera as camera
+import mmSolver._api.markergroup as markergroup
+import mmSolver._api.marker as marker
+
+
+# @unittest.skip
+class TestMarkerGroup(test_api_utils.APITestCase):
+
+    @staticmethod
+    def create_camera(name):
+        cam_tfm = maya.cmds.createNode('transform', name=name)
+        cam_tfm = api_utils.get_long_name(cam_tfm)
+        cam_shp = maya.cmds.createNode('camera', name=name+'Shape',
+                                       parent=cam_tfm)
+        cam_shp = api_utils.get_long_name(cam_shp)
+        cam = camera.Camera(transform=cam_tfm, shape=cam_shp)
+        return cam
+
+    def test_init(self):
+        x = markergroup.MarkerGroup()
+        self.assertEqual(x.get_node(), None)
+        self.assertIsInstance(x, markergroup.MarkerGroup)
+        self.assertFalse(x.is_valid())
+
+    def test_set_node(self):
+        x = markergroup.MarkerGroup()
+        self.assertEqual(x.get_node(), None)
+        self.assertFalse(x.is_valid())
+
+        cam = self.create_camera('camera')
+        cam_tfm = cam.get_transform_node()
+        node = maya.cmds.createNode('mmMarkerGroupTransform',
+                                    name='markerGroup1',
+                                    parent=cam_tfm)
+        x.set_node(node)
+        self.assertEqual(x.get_node(), cam_tfm + '|markerGroup1')
+        self.assertTrue(x.is_valid())
+
+    def test_create_node(self):
+        cam = self.create_camera('myCamera')
+        cam_tfm = cam.get_transform_node()
+        x = markergroup.MarkerGroup().create_node(cam=cam)
+        self.assertIsInstance(x, markergroup.MarkerGroup)
+        self.assertEqual(x.get_node(), cam_tfm + '|markerGroup1')
+        self.assertTrue(x.is_valid())
+
+if __name__ == '__main__':
+    prog = unittest.main()

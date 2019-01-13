ec15049eb9ee03cbc30dc26014ca8a8cec6c35f0
David Cattermole <cattermole91@gmail.com>
2019-01-13 18:52:48 +0000

    Use 'tool' for load marker, instead of 'ui' module file.

--- python/mmSolver/tools/loadmarker/tool.py
+++ python/mmSolver/tools/loadmarker/tool.py
@@ -5,13 +5,18 @@ The Load Marker tool - user facing.
 import maya.cmds
 import mmSolver.api as mmapi
 import mmSolver.logger
-import mmSolver.tools.selection.filternodes as filternodes
-import mmSolver.tools.loadmarker.formatmanager as formatmanager
-# import mmSolver.tools.loadmarker.lib as lib
+# import mmSolver.tools.selection.filternodes as filternodes
+# import mmSolver.tools.loadmarker.formatmanager as formatmanager
 
 
 LOG = mmSolver.logger.get_logger()
 
 
-def main(file_path, cam=None, **kwargs):
-    raise NotImplementedError
+# def main(file_path, cam=None, **kwargs):
+#     raise NotImplementedError
+
+
+def open_window():
+    mmapi.load_plugin()
+    import mmSolver.tools.loadmarker.ui.loadmarker_window as loadmarker_window
+    loadmarker_window.main()

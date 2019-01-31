e26c14493f95b6759860c73e507f6c60a991c7d0
David Cattermole <cattermole91@gmail.com>
2019-01-31 22:18:45 +0000

    Bug fix: ignores markers with no tracked data. Added test case files for .uv (version 2) Marker loader.

--- tests/test/test_tools/test_loadmarker.py
+++ tests/test/test_tools/test_loadmarker.py
@@ -54,13 +54,14 @@ class TestLoadMarker(test_tools_utils.ToolsTestCase):
     def test_loadmarker_uvtrack_format(self):
         """
         Test loading markers using the '.uv' format.
-
-        TODO: Make sure to test both formats, format 1 and 2.
         """
         mkr_data_list = []
         paths = [
             self.get_data_path('uvtrack', 'test_v1.uv'),
             self.get_data_path('uvtrack', 'loadmarker_corners.uv'),
+            self.get_data_path('uvtrack', 'cameraTrackRnD.uv'),
+            self.get_data_path('uvtrack', 'stA.uv'),
+            self.get_data_path('uvtrack', 'stA_with_emptyMarker.uv'),
         ]
         for path in paths:
             print('Reading... %r' % path)

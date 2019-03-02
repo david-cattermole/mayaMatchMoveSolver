da139cb83172151f2fcb42b374f86138d4ebae2f
David Cattermole <cattermole91@gmail.com>
2019-03-02 14:31:01 +0000

    Changed duplicated marker node name, so it's prefixed with 'dup' and users previous marker/bundle node name.

--- python/mmSolver/tools/duplicatemarker/tool.py
+++ python/mmSolver/tools/duplicatemarker/tool.py
@@ -34,13 +34,33 @@ def main():
 
     new_mkr_nodes = []
     for marker in selected_markers:
+        # Get Marker's name
+        mkr = mmapi.Marker(name=marker)
+        old_mkr_node = mkr.get_node()
+        if old_mkr_node is None:
+            LOG.warning('Invalid Marker, skipping duplicate.')
+            continue
+        mkr_name = old_mkr_node.rpartition('|')
+        mkr_name = 'dup' + mkr_name.capitalize()
+
+        # Get Bundles's name
+        bnd_name = 'dupBundle1'
+        bnd = mkr.get_bundle()
+        if bnd is None:
+            pass
+        else:
+            bnd_node = bnd.get_node()
+            bnd_name = bnd_node.rpartition('|')
+            bnd_name = 'dup' + bnd_name.capitalize()
+
         # get attrs lock state
         lock_value = lib.__get_lock_state(marker, mkr_attrs)
-        mkr_name = mmapi.get_marker_name('marker1', suffix='_DUP')
+
+        mkr_name = mmapi.get_marker_name(mkr_name)
         new_mkr = mmapi.Marker().create_node(cam=cam_from_mkr,
                                              name=mkr_name)
         new_mkr_node = new_mkr.get_node()
-        bnd_name = mmapi.get_bundle_name('bundle1', suffix='_DUP')
+        bnd_name = mmapi.get_bundle_name(bnd_name)
         new_bnd = mmapi.Bundle().create_node(name=bnd_name)
         # connecting bundle to the marker
         new_mkr.set_bundle(new_bnd)

cf8542b2ddda6c827ead797b253a007d7bc5241a
David Cattermole <cattermole91@gmail.com>
2019-03-10 01:24:27 +0000

    Changed smooth keyframes tool to work on all selected curves. Added warning messages to smooth keyframes tool.

--- python/mmSolver/tools/smoothkeyframes/tool.py
+++ python/mmSolver/tools/smoothkeyframes/tool.py
@@ -4,8 +4,11 @@ Smooths the selected keyframes.
 
 import maya.cmds
 
+import mmSolver.logger
 import mmSolver.utils.constant as utils_const
 import mmSolver.utils.smooth as utils_smooth
+
+LOG = mmSolver.logger.get_logger()
 
 
 def main():
@@ -17,24 +20,44 @@ def main():
     2) Run tool
     3) Keyframe values will be smoothed.
     """
-    key_attr = maya.cmds.keyframe(query=True, name=True)
-    selected_keyframes = maya.cmds.keyframe(query=True, selected=True) or []
+    key_attrs = maya.cmds.keyframe(query=True, name=True) or []
+    if len(key_attrs) == 0:
+        msg = (
+            'Please select keyframes (in the'
+            ' Graph Editor) to smooth.'
+        )
+        LOG.warning(msg)
+        return
+    
+    for key_attr in key_attrs:
+        selected_keyframes = maya.cmds.keyframe(
+            key_attr,
+            query=True,
+            selected=True
+        ) or []
+        if len(selected_keyframes) < 2:
+            msg = (
+                'Please select more than 1 keyframes '
+                '(in the Graph Editor) to smooth.'
+            )
+            LOG.warning(msg)
+            return
 
-    value_array = []
-    for frame in selected_keyframes:
-        plug = key_attr[0] + '.output'
-        value = maya.cmds.getAttr(plug, time=frame)
-        value_array.append(value)
+        value_array = []
+        for frame in selected_keyframes:
+            plug = key_attr + '.output'
+            value = maya.cmds.getAttr(plug, time=frame)
+            value_array.append(value)
 
-    smooth_type = utils_const.SMOOTH_TYPE_FOURIER
-    width = 2
-    new_array = utils_smooth.smooth(smooth_type, value_array, width)
+        smooth_type = utils_const.SMOOTH_TYPE_FOURIER
+        width = 2
+        new_array = utils_smooth.smooth(smooth_type, value_array, width)
 
-    assert len(selected_keyframes) == len(new_array)
-    for frame, value in zip(selected_keyframes, new_array):
-        time_range = (frame, frame)
-        maya.cmds.keyframe(
-            key_attr[0],
-            valueChange=value,
-            time=time_range)
+        assert len(selected_keyframes) == len(new_array)
+        for frame, value in zip(selected_keyframes, new_array):
+            time_range = (frame, frame)
+            maya.cmds.keyframe(
+                key_attr,
+                valueChange=value,
+                time=time_range)
     return

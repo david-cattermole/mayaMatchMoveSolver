d031fc2c731c08e39f41d2fa540e0f8911b16dc5
David Cattermole <cattermole91@gmail.com>
2019-03-10 01:12:43 +0000

    Fixed fourier smoothing; window is now always odd-sized-length, so it centres around the current value. Fixed triangle fourier filter, it is now centred around the middle. Added unit tests for accuracy.

--- tests/test/test_utils/test_smooth.py
+++ tests/test/test_utils/test_smooth.py
@@ -4,9 +4,6 @@ Test functions for API utils module.
 
 import unittest
 
-# import maya.cmds
-# import maya.OpenMaya as OpenMaya
-
 import test.test_utils.utilsutils as test_utils
 import mmSolver.utils.smooth as smooth_utils
 
@@ -19,6 +16,16 @@ DATA_ONE = [
     0.24322363800544966
 ]
 
+# A single spike (length 5 - odd)
+DATA_TWO = [
+    0.0, 0.0, 1.0, 0.0, 0.0, 
+]
+
+# A single spike (length 8 - even).
+DATA_THREE = [
+    0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 
+]
+
 
 # @unittest.skip
 class TestSmooth(test_utils.UtilsTestCase):
@@ -26,7 +33,7 @@ class TestSmooth(test_utils.UtilsTestCase):
     Test smooth module.
     """
 
-    def test_average_smooth(self):
+    def test_average_smooth_one(self):
         """
         Test the average smoothing function.
         """
@@ -50,7 +57,7 @@ class TestSmooth(test_utils.UtilsTestCase):
             self.assertEqual(i, x1)
         return
 
-    def test_gaussian_smooth(self):
+    def test_gaussian_smooth_one(self):
         """
         Test the gaussian smoothing function.
         """
@@ -65,9 +72,9 @@ class TestSmooth(test_utils.UtilsTestCase):
         self.assertNotEqual(data, x)
         return
 
-    def test_fourier_smooth(self):
+    def test_fourier_smooth_one(self):
         """
-        Test the fourier smoothing function.
+        Test the fourier smoothing function, with data one input.
 
         .. todo:: 
 
@@ -89,6 +96,96 @@ class TestSmooth(test_utils.UtilsTestCase):
                 same_value = False
                 break
         self.assertIs(same_value, False)
+        return
+
+    def test_fourier_smooth_two(self):
+        """
+        Test the fourier smoothing function, with data input two
+        """
+        # No change should happen with a 'width' of 1.0.
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='box')
+        self.assertEqual(data, x)
+
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='triangle')
+        self.assertEqual(data, x)
+
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='fourier')
+        self.assertEqual(data, x)
+
+        # Smooth by two frames.
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='box')
+        data = [
+            0.0, 0.3333333333333333, 0.3333333333333333, 0.3333333333333333, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
+
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='triangle')
+        data = [
+            0.0, 0.2, 0.6, 0.2, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
+
+        data = list(DATA_TWO)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='gaussian')
+        data = [
+            
+            0.0, 0.27406862, 0.45186276, 0.27406862, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
+        return
+
+    def test_fourier_smooth_three(self):
+        """
+        Test the fourier smoothing function, with data input three
+        """
+        # No change should happen with a 'width' of 1.0.
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='box')
+        self.assertEqual(data, x)
+
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='triangle')
+        self.assertEqual(data, x)
+
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 1.0, filtr='gaussian')
+        self.assertEqual(data, x)
+
+        # Smooth by two frames.
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='box')
+        data = [
+            0.0, 0.0, 0.3333333333333, 0.66666666666666,
+            0.66666666666666, 0.3333333333333, 0.0, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
+
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='triangle')
+        data = [
+            0.0, 0.0, 0.2, 0.8,
+            0.8, 0.2, 0.0, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
+
+        data = list(DATA_THREE)
+        x = smooth_utils.fourier_smooth(data, 2.0, filtr='gaussian')
+        data = [
+            0.0, 0.0, 0.27406862, 0.72593138,
+            0.72593138, 0.27406862, 0.0, 0.0,
+        ]
+        for a, b in zip(list(x), data):
+            self.assertAlmostEqual(a, b)
         return
 
 

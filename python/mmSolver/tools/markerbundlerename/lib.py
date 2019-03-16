8e035d8dbdf4357f0b3e5bcf8ba4bfa819d4caf9
anilreddy24fps <anilreddy24fps@gmail.com>
2019-03-16 23:02:47 +1030

    renamed folder and created lib file

---
+++ python/mmSolver/tools/markerbundlerename/lib.py
@@ -0,0 +1,36 @@
+"""
+This file holds all the helpful functions for markerbundlerename
+"""
+
+
+def prompt_for_new_node_name(title, message, text):
+    """
+    Ask the user for a new node name.
+
+    :param title: Dialog box window title.
+    :type title: str
+
+    :param message: Read-only text to show the user, for making a
+                    decision.
+    :type message: str
+
+    :param text: The initial text to prompt the user as a starting
+                 point.
+    :type text: str
+
+    :return: New node name, or None if user cancelled.
+    :rtype: str or None
+    """
+    name = None
+    result = maya.cmds.promptDialog(
+        title=title,
+        message=message,
+        text=text,
+        button=['OK', 'Cancel'],
+        defaultButton='OK',
+        cancelButton='Cancel',
+        dismissString='Cancel',
+    )
+    if result == 'OK':
+        name = maya.cmds.promptDialog(query=True, text=True)
+    return name

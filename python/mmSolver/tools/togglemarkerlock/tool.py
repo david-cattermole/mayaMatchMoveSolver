aa0f97183c8566ab71ab8b50044208c2f79ff93d
anilreddy24fps <anilreddy24fps@gmail.com>
2019-04-09 21:31:05 +0930

    added toggle marker lock tool, documentation and a shelf button under MTools popup menu

---
+++ python/mmSolver/tools/togglemarkerlock/tool.py
@@ -0,0 +1,37 @@
+"""
+This tool toggles selected marker lock state
+"""
+
+import maya.cmds
+import mmSolver.logger
+import mmSolver.tools.selection.filternodes as filternodes
+import mmSolver.tools.togglemarkerlock.constant as const
+
+LOG = mmSolver.logger.get_logger()
+
+
+def toggle_marker_lock():
+    """
+    Toggles selected marker lock state.
+    """
+    selection = maya.cmds.ls(selection=True, long=True) or []
+    selected_markers = filternodes.get_marker_nodes(selection)
+    if len(selected_markers) == 0:
+        LOG.warning("Please select marker's to lock or unlock")
+        return
+
+    attrs = const.ATTRS
+    marker_attrs = []
+    for marker in selected_markers:
+        for attr in attrs:
+            marker_attrs.append('%s.%s' % (marker, attr))
+
+    is_locked = False
+    for attr in marker_attrs:
+        if maya.cmds.getAttr(attr, lock=True):
+            is_locked = True
+
+    for attr in marker_attrs:
+        lock_value = not is_locked
+        maya.cmds.setAttr(attr, lock=lock_value)
+    return

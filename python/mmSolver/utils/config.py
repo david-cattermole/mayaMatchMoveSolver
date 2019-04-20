02b51e9c96e5f65d1ddbc9afdde3a13d899c772d
David Cattermole <cattermole91@gmail.com>
2019-04-20 14:39:57 +0100

    Changed config function signature. Issue #17. Removed use of Maya commands in config.py. Changed configuration environment variable from MMSOLVER_CONFIG_DIR to MMSOLVER_CONFIG_PATH.

--- python/mmSolver/utils/config.py
+++ python/mmSolver/utils/config.py
@@ -3,49 +3,48 @@ Configuration module.
 
 GitHub issue #17.
 
-A few of our tools need to have config files to read from and write to (hotkey manager, shelf and menus)
+A few of our tools need to have config files to read from and write to
+(hotkey manager, shelf and menus).
 
-There are many places to store and read data from in Maya.
+There are many places to store and read data within Maya.
 
-- A Maya node
-- The Maya scene file.
-- The Maya preferences
 - The user's home directory (by-passing the Maya preferences)
 - Configuration options in the install Maya module (configured for everyone).
 
 This module needs to handle a number of use cases:
 
 - Module to read/write config values.
-- Store data in the scene file.
-- Store data on a Maya node.
 - Store data in the user's home directory.
-- Store data in the Maya preferences.
-- Read data from the config directory "MM_SOLVER_CONFIG"
+- Read data from the config directory "MMSOLVER_CONFIG_PATH"
+
 """
 
+import os
 import json
 
-import maya.cmds
 import mmSolver.logger
-
+import mmSolver.utils.constant as const
 
 LOG = mmSolver.logger.get_logger()
 
 
-class Config(object):
-    pass
-
-
-def get_config_directories():
+def get_dirs(envvar_name=None):
     """
     Get a list of directories to look in for config files.
 
-    :return:
+    :param envvar_name: The environment variable to query for a list 
+                        of directories.
+    :type envvar_name: str or None
+
+    :return: List of config directories.
     """
-    pass
+    if envvar_name is None:
+        envvar_name = const.CONFIG_PATH_VAR_NAME
+    result = []
+    return result
 
 
-def find_config_path(file_name):
+def find_path(file_name, search_paths):
     """
     Search though a list of defined paths for the config file name given.
 
@@ -60,51 +59,55 @@ def find_config_path(file_name):
     pass
 
 
-def read_config(file_name):
+def read_data(file_path):
     """
-    Read configuration file.
+    Read configuration file and return the data embedded.
 
-    :param file_name: The name of the config file, or an absolute file
-                      path.
-    :type file_name: str
+    :param file_path: The absolute file path to a config file.
+    :type file_path: str
 
-    :return:
-    """
-    pass
-
-
-def get_config(file_name):
-    """
-    Read a config file.
-
-    :param file_name: The name of the config file, or an absolute file
-                      path.
-    :type file_name: str
-
-    :return:
-    :rtype:
+    :return: Dictionary, list or None, depending what the given file 
+             contains.
+    :rtype: dict, list or None
     """
     pass
 
 
-def get_config_value(file_name, default_value=None, *args):
+def write_data(data, file_path):
     """
-    Get a value from the config file.
+    Write the given configuration data to a file.
 
-    If the config file does not exist, or one of the argument (keys)
+    :param data: Configuration data to write. The data should be 
+                 plain-old-(Python)data types, like lists, float, int, 
+                 string and dict.
+    :type data: dict or list
+
+    :param file_path: A valid absolute file path.
+    :type file_path: str
+    """
+    pass
+
+
+def get_value(data, key, default_value=None):
+    """
+    Get a value from the config data.
+
+    If the config data value does not exist, or one of the argument (keys)
     does not exist, 'default_value' is returned.
 
-    >>> x = get_config_value('config.json', 'key')
-    >>> y = get_config_value('config.json', 'key', 'subkey')
-    >>> z = get_config_value('config.json', 'key', 'subkey', 'subsubkey')
+    >>> data = read_data('/path/to/config.json')
+    >>> x = get_value(data, 'key')
+    >>> y = get_value(data, 'key/subkey')
+    >>> z = get_value(data, 'key/subkey/subsubkey')
 
     :param file_name: The name of the config file, or an absolute file
                       path.
     :type file_name: str
 
-    :param args: Hierarchy of keys to search for in the config.
-                 For example 'config_data[arg1][arg2][arg3]'.
-    :type args: str
+    :param key: Hierarchy of keys separated by forward slash to search 
+                for in the config. 
+                For example 'data[arg1][arg2][arg3]'.
+    :type key: str
 
     :param default_value: The value returned when the config file or
                           key doesn't exist.
@@ -116,105 +119,60 @@ def get_config_value(file_name, default_value=None, *args):
     pass
 
 
-def set_config_value():
+def set_value(data, key, value):
+    """
+    Set a value in the given configuration data.
+
+    :returns: A copy of the configuration data with the modification 
+              made.
+    :rtype: dict or list
+    """
     pass
 
 
-def get_data_on_node_attr(node_name, attr_name):
+class Config(object):
+
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.values = {}
+        self._autoread = True
+        self._autowrite = True
+
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
+    def get_value(self):
+        pass
+
+    def set_value(self):
+        pass
+
+    def get_autoread(self):
+        pass
+
+    def set_autoread(self):
+        pass
+
+    def get_autowrite(self):
+        pass
+
+    def set_autowrite(self):
+        pass
+
+
+def get_config(file_name, envvar=None):
     """
-    Get data from an node attribute.
+    Read a config file as a Config class object.
 
-    :param node_name: Node to get data from.
-    :type node_name: str
+    :param file_name: The name of the config file, or an absolute file
+                      path.
+    :type file_name: str
 
-    :param attr_name: The name of the attribute to get data from.
-    :type attr_name: str
-
-    :return: Arbitrary Plain-Old-Data data structures.
-    :rtype: list of dict
+    :return: Config object.
+    :rtype: Config
     """
-    ret = []
-    attr_data = get_value_on_node_attr(node_name, attr_name)
-    if attr_data is None:
-        return ret
-    data = json.loads(attr_data)
-    if isinstance(data, list):
-        ret = data
-    return ret
-
-
-def set_data_on_node_attr(node_name, attr_name, data):
-    """
-    Set arbitrary Plain-Old-Data onto a node.attr path.
-
-    :param node_name: Node to store data on.
-    :type node_name: str
-
-    :param attr_name: Attribute name to store data with.
-    :type attr_name: str
-
-    :param data: The data to store.
-    :type data: list or dict
-    """
-    assert isinstance(attr_name, (str, unicode))
-    assert isinstance(data, (list, dict))
-    node_attr = node_name + '.' + attr_name
-
-    new_attr_data = json.dumps(data)
-    old_attr_data = maya.cmds.getAttr(node_attr)
-    if old_attr_data == new_attr_data:
-        return  # no change is needed.
-
-    maya.cmds.setAttr(node_attr, lock=False)
-    maya.cmds.setAttr(node_attr, new_attr_data, type='string')
-    maya.cmds.setAttr(node_attr, lock=True)
-    return
-
-
-def get_value_on_node_attr(node_name, attr_name):
-    """
-    Get numeric value from an node attribute.
-
-    :param node_name: Node to get value from.
-    :type node_name: str
-
-    :param attr_name: The name of the attribute to get value from.
-    :type attr_name: str
-
-    :return: A numeric value.
-    :rtype: bool or float or int
-    """
-    attrs = maya.cmds.listAttr(node_name)
-    if attr_name not in attrs:
-        msg = 'attr_name not found on node: '
-        msg += 'attr_name={name} node={node}'
-        msg = msg.format(name=attr_name, node=node_name)
-        raise ValueError(msg)
-    node_attr = node_name + '.' + attr_name
-    ret = maya.cmds.getAttr(node_attr)
-    return ret
-
-
-def set_value_on_node_attr(node_name, attr_name, data):
-    """
-    Set value onto a node.attr path.
-
-    :param node_name: Node to store value on.
-    :type node_name: str
-
-    :param attr_name: Attribute name to store value with.
-    :type attr_name: str
-
-    :param data: The numeric value to store.
-    :type data: bool or float or int
-
-    ;return: Nothing.
-    :rtype: None
-    """
-    assert isinstance(attr_name, (str, unicode))
-    assert isinstance(data, (bool, float, int))
-    node_attr = node_name + '.' + attr_name
-    maya.cmds.setAttr(node_attr, lock=False)
-    maya.cmds.setAttr(node_attr, data)
-    maya.cmds.setAttr(node_attr, lock=True)
-    return
+    pass

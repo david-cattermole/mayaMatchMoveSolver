6deba8d35d1d594e37bc999f8e7c4e763bc0c953
david-cattermole <cattermole91@gmail.com>
2019-05-09 23:14:11 +0100

    Added simple Maya menu creation. Issue #16. Fixed bug in mmSolver.ui.shelfutils.create_menu_item, error if command flag was not given.

--- python/mmSolver/tools/mmmenu/tool.py
+++ python/mmSolver/tools/mmmenu/tool.py
@@ -4,6 +4,7 @@ Build the mmSolver menu.
 
 import mmSolver.logger
 import mmSolver.tools.mmmenu.constant as const
+import mmSolver.ui.menuutils as menu_utils
 
 
 LOG = mmSolver.logger.get_logger()
@@ -13,4 +14,9 @@ def build_menu():
     """
     Build the 'mmSolver' menu.
     """
-    return
+    parent = menu_utils.get_maya_window_parent()
+    mm_menu = menu_utils.create_menu(parent=parent, name=const.MENU_NAME)
+    for i in range(10):
+        name = 'name' + str(i)
+        item = menu_utils.create_menu_item(parent=mm_menu, name=name)
+    return mm_menu

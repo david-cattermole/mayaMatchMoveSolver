david-cattermole/mayaMatchMoveSolver

commit a759ef05d4527f626d4c549a17af14c29b08a729

Each node/attribute UI object has a list of attributes it represents.

python/mmSolver/_api/collection.py
@@ -504,9 +504,10 @@ class Collection(object):
     def remove_attribute_list(self, attr_list):
         assert isinstance(attr_list, list)
         name_list = []
-        for attr in attr_list:
-            if isinstance(attr, attribute.Attribute):
-                name_list.append(attr.get_name())
+        for attrs in attr_list:
+            for attr in attrs:
+                if isinstance(attr, attribute.Attribute):
+                    name_list.append(attr.get_name())
         self._set.remove_members(name_list)
         self._actions_list = []  # reset argument flag cache.
         return

python/mmSolver/tools/solver/lib/attr.py
@@ -213,17 +213,18 @@ def add_callbacks_to_attributes(attr_list, update_func, callback_manager):
 def remove_callbacks_from_attributes(attr_list, callback_manager):
     msg = 'Node UUID has multiple paths: node=%r node_uuids=%r'
     callback_type = maya_callbacks.TYPE_ATTRIBUTE
-    for attr_obj in attr_list:
-        node_path = attr_obj.get_node(full_path=True)
-        node_uuids = maya.cmds.ls(node_path, uuid=True) or []
-        if len(node_uuids) != 1:
-            LOG.debug(msg, node_path, node_uuids)
-            continue
-        node_uuid = node_uuids[0]
-        if callback_manager.type_has_node(callback_type, node_uuid) is False:
-            continue
-        callback_manager.remove_type_node_ids(
-            callback_type,
-            node_uuid,
-        )
+    for attr_objs in attr_list:
+        for attr_obj in attr_objs:
+            node_path = attr_obj.get_node(full_path=True)
+            node_uuids = maya.cmds.ls(node_path, uuid=True) or []
+            if len(node_uuids) != 1:
+                LOG.debug(msg, node_path, node_uuids)
+                continue
+            node_uuid = node_uuids[0]
+            if callback_manager.type_has_node(callback_type, node_uuid) is False:
+                continue
+            callback_manager.remove_type_node_ids(
+                callback_type,
+                node_uuid,
+            )
     return

python/mmSolver/tools/solver/lib/uiquery.py
@@ -74,8 +74,8 @@ def convert_ui_nodes_to_nodes(ui_nodes, key):
         node_data = ui_node.data()
         if node_data is None:
             continue
-        mkr_node = node_data.get(key)
-        if mkr_node is None:
+        data_content = node_data.get(key)
+        if data_content is None:
             continue
-        nodes.append(mkr_node)
+        nodes.append(data_content)
     return nodes

python/mmSolver/tools/solver/widget/attribute_widget.py
@@ -265,6 +265,9 @@ class AttributeBrowserWidget(nodebrowser_widget.NodeBrowserWidget):
     def addClicked(self):
         """
         Add the selected nodes or node attributes to the data model.
+
+        .. todo:: This function is very slow to run when many (20+)
+           bundles are being added to the list attributes.
         """
         col = lib_state.get_active_collection()
         if col is None:

commit 6a5bd78b7f58095123cece9be3c32cbbd4b2dc9f

Fix incorrect assumption of Attribute list introduced in commit a759ef05, issue #65.

python/mmSolver/_api/collection.py
@@ -504,10 +504,9 @@ class Collection(object):
     def remove_attribute_list(self, attr_list):
         assert isinstance(attr_list, list)
         name_list = []
-        for attrs in attr_list:
-            for attr in attrs:
-                if isinstance(attr, attribute.Attribute):
-                    name_list.append(attr.get_name())
+        for attr in attr_list:
+            if isinstance(attr, attribute.Attribute):
+                name_list.append(attr.get_name())
         self._set.remove_members(name_list)
         self._actions_list = []  # reset argument flag cache.
         return
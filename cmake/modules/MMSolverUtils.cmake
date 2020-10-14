0de6852b5ba37d7d718bc2bef08314c1afa8817f
David Cattermole <dcatterm@Davids-iMac.local>
2020-10-14 16:55:59 +1030

    Add missing copyright preamble for utility file. Issue #150.

--- cmake/modules/MMSolverUtils.cmake
+++ cmake/modules/MMSolverUtils.cmake
@@ -1,3 +1,24 @@
+# Copyright (C) 2020 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+#
+# CMake utilities for mmSolver.
+#
+
 function(add_target_link_library_names target names)
     string(STRIP ${names} names_strip)
     string(REPLACE " " ";" names_list ${names_strip})

a0dcf94e0b90f184310f91747eb710bf096ba71a
ktonegawa <kazuma.tonegawa@gmail.com>
2020-12-07 18:28:10 -0800

    adding Remove Solver Nodes tool in an attempt to tackle this issue: https://github.com/david-cattermole/mayaMatchMoveSolver/issues/76

---
+++ python/mmSolver/tools/removesolvernodes/ui/removesolvernodes_window.py
@@ -0,0 +1,119 @@
+"""
+Window for the Remove Solver Nodes tool.
+
+Usage::
+
+   import mmSolver.tools.removesolvernodes.ui.removesolvernodes_window as removesolvernodes_window
+   removesolvernodes_window.main()
+
+"""
+
+import mmSolver.ui.qtpyutils as qtpyutils
+qtpyutils.override_binding_order()
+
+import Qt.QtCore as QtCore
+import Qt.QtGui as QtGui
+import Qt.QtWidgets as QtWidgets
+
+
+import mmSolver.logger
+import mmSolver.ui.uiutils as uiutils
+import mmSolver.ui.helputils as helputils
+import mmSolver.tools.removesolvernodes.constants as const
+import mmSolver.tools.removesolvernodes.tool as tool
+import mmSolver.tools.removesolvernodes.ui.removesolvernodes_layout as removesolvernodes_layout
+
+
+LOG = mmSolver.logger.get_logger()
+baseModule, BaseWindow = uiutils.getBaseWindow()
+
+
+class RemoveSolverNodesWindow(BaseWindow):
+    name = 'RemoveSolverNodesWindow'
+
+    def __init__(self, parent=None, name=None):
+        super(RemoveSolverNodesWindow, self).__init__(parent,
+                                               name=name)
+        self.setupUi(self)
+        self.addSubForm(removesolvernodes_layout.RemoveSolverNodesLayout)
+
+        self.setWindowTitle(const.WINDOW_TITLE)
+        self.setWindowFlags(QtCore.Qt.Tool)
+
+        # Standard Buttons
+        self.baseHideStandardButtons()
+        # self.applyBtn.show()
+        self.resetBtn.show()
+        self.helpBtn.show()
+        self.closeBtn.show()
+        # self.applyBtn.setText('Smooth')
+        self.subForm.clean_pushButton.clicked.connect(self.clean)
+
+        # self.applyBtn.clicked.connect(tool.smooth_selected_keyframes)
+        # self.resetBtn.clicked.connect(self.reset_options)
+        # self.helpBtn.clicked.connect(self.help)
+
+        # Hide irrelevant stuff
+        self.baseHideMenuBar()
+        self.baseHideProgressBar()
+
+    def reset_options(self):
+        form = self.getSubForm()
+        form.reset_options()
+        return
+
+    def clean(self):
+        what_to_delete_dict = {
+            'markers': self.subForm.markers_checkBox.isChecked(),
+            'bundles': self.subForm.bundles_checkBox.isChecked(),
+            'marker_groups': self.subForm.markerGroup_checkBox.isChecked(),
+            'collections': self.subForm.collections_checkBox.isChecked(),
+            'other_nodes': self.subForm.otherNodes_checkBox.isChecked()
+            }
+        all_list_to_delete, unknown_node_found = tool.filter_nodes(
+                                                        what_to_delete_dict
+                                                    )
+        print all_list_to_delete
+        print 'unknown_node_found: ', unknown_node_found
+        if unknown_node_found:
+            title = 'Confirmation'
+            msg = 'Non-bundle nodes found parented under one or more bundle\n'\
+                'nodes. It is suggested that you unparent and bake nodes \n'\
+                'you want to keep.\n'\
+                'Are you sure you want to delete them?'
+            clicked_button = QtWidgets.QMessageBox.question(self, title, msg)
+            if clicked_button == QtWidgets.QMessageBox.No:
+                return
+        tool.delete_nodes(all_list_to_delete)
+
+    def help(self):
+        src = helputils.get_help_source()
+        page = 'tools.html#smooth-keyframes'
+        helputils.open_help_in_browser(page=page, help_source=src)
+        return
+
+
+def main(show=True, auto_raise=True, delete=False):
+    """
+    Open the Smooth Keyframes UI window.
+
+    :param show: Show the UI.
+    :type show: bool
+
+    :param auto_raise: If the UI is open, raise it to the front?
+    :type auto_raise: bool
+
+    :param delete: Delete the existing UI and rebuild it? Helpful when
+                   developing the UI in Maya script editor.
+    :type delete: bool
+
+    :returns: A new solver window, or None if the window cannot be
+              opened.
+    :rtype: SolverWindow or None.
+    """
+    win = RemoveSolverNodesWindow.open_window(
+        show=show,
+        auto_raise=auto_raise,
+        delete=delete
+    )
+    return win

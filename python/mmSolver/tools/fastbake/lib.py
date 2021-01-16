b23cc90bf0b3e5f5c6da34fda96e409952e70843
bpatchasaheb <patchasaheb@gmail.com>
2021-01-16 06:33:45 -0500

    unwanted argument deleted from lib file

---
+++ python/mmSolver/tools/fastbake/lib.py
@@ -0,0 +1,48 @@
+# Copyright (C) 2021 Patcha Saheb Binginapalli.
+
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+import mmSolver.logger
+import mmSolver.utils.tools as tools_utils
+
+
+import maya.cmds as cmds
+import maya.mel as mel
+
+LOG = mmSolver.logger.get_logger()
+
+def bakeAttributes(nodes, start_frame, end_frame, smart_bake, attrs):
+    try:
+        cmds.refresh(suspend=True)
+        mel.eval("paneLayout -e -manage false $gMainPane")
+        ctx = tools_utils.tool_context(
+            use_undo_chunk=True,
+            restore_current_frame=True,
+            use_dg_evaluation_mode=True,
+            disable_viewport=False)
+        with ctx:
+            for node in nodes:
+                if smart_bake == True:
+                    cmds.bakeResults(node, time=(start_frame, end_frame), simulation=True, smart=True, attribute=attrs)
+                else:
+                    cmds.bakeResults(node, time=(start_frame, end_frame), simulation=True, attribute=attrs)
+    except(NameError, ValueError, TypeError) as e:
+        print e            
+    finally:
+        mel.eval("paneLayout -e -manage true $gMainPane")
+        cmds.refresh(suspend=False)
+

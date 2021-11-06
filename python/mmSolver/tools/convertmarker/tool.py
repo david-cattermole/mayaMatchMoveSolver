ee0f8294491897f29f862e557061a9d6dc0c5749
David Cattermole <cattermole91@gmail.com>
2021-11-07 02:00:27 +1100

    "Convert to Marker" tool can bake the bundle position as the source transform.
    Issue #227.

--- python/mmSolver/tools/convertmarker/tool.py
+++ python/mmSolver/tools/convertmarker/tool.py
@@ -36,11 +36,60 @@ import mmSolver.tools.convertmarker.lib as lib
 
 LOG = mmSolver.logger.get_logger()
 
+BUNDLE_POSITION_MODE_ORIGIN = 'origin'  # Bundle stays at the origin.
+BUNDLE_POSITION_MODE_SOURCE = 'source'  # Bundle moves to the source transform.
 
-def main():
+
+def _match_node_position(nodes, target_nodes, start_frame, end_frame, delete_static_anim_curves):
+    """Move the 'nodes' to the position of the 'target_nodes'.
+
+    'nodes' and 'target_nodes' are expected to be lists of equal
+    length, each list index corresponds to the other list.
     """
-    Convert all selected transforms into 2D markers under a camera.
+    constraint_nodes = []
+    for target_node, node in zip(target_nodes, nodes):
+        constraint_nodes += maya.cmds.pointConstraint(
+            target_node, node,
+            maintainOffset=False)
+
+    # Bake and delete the constraints.
+    if len(constraint_nodes) > 0:
+        frame_range = (float(start_frame), float(end_frame),)
+        maya.cmds.bakeResults(
+            nodes,
+            time=frame_range,
+            sampleBy=1,
+            sparseAnimCurveBake=False,
+            minimizeRotation=True,
+            disableImplicitControl=True,
+            preserveOutsideKeys=True,
+            controlPoints=False,
+            shape=False)
+        maya.cmds.delete(nodes, constraints=True)
+        maya.cmds.filterCurve(nodes)
+
+        if delete_static_anim_curves is True:
+            maya.cmds.delete(nodes, staticChannels=True)
+    return
+
+
+def main(bundle_position_mode=None, delete_static_anim_curves=None):
+    """Convert all selected transforms into 2D markers under a camera.
+
+    :param bundle_position_mode: The position for the newly created
+       Bundle (connected to the Marker).
+    :type bundle_position_mode: None or BUNDLE_POSITION_MODE_*
+
+    :param delete_static_anim_curves: When enabled, this will remove
+       all keyframes from the bundle, if the bundle is not animated.
+    :type delete_static_anim_curves: bool
+
     """
+    if bundle_position_mode is None:
+        bundle_position_mode = BUNDLE_POSITION_MODE_ORIGIN
+    if delete_static_anim_curves is None:
+        delete_static_anim_curves = True
+
     # Get camera
     model_editor = utils_viewport.get_active_model_editor()
     if model_editor is None:
@@ -68,9 +117,9 @@ def main():
 
     mmapi.load_plugin()
 
+    start_frame, end_frame = utils_time.get_maya_timeline_range_outer()
     with tools_utils.tool_context(pre_update_frame=True):
         # Compute the Marker Data.
-        start_frame, end_frame = utils_time.get_maya_timeline_range_outer()
         mkr_data_list = lib.convert_nodes_to_marker_data_list(
             cam_tfm,
             cam_shp,
@@ -100,6 +149,21 @@ def main():
             with_bundles=True,
         )
         mkr_nodes = [mkr.get_node() for mkr in mkr_list]
+
+        if bundle_position_mode == BUNDLE_POSITION_MODE_ORIGIN:
+            # Bundle stays at the origin.
+            pass
+        elif bundle_position_mode == BUNDLE_POSITION_MODE_SOURCE:
+            # Move the newly created bundle to the original transform's
+            # location.
+            bnd_list = [mkr.get_bundle() for mkr in mkr_list]
+            bnd_list = [bnd for bnd in bnd_list if bnd is not None]
+            bnd_nodes = [bnd.get_node() for bnd in bnd_list]
+            _match_node_position(
+                bnd_nodes, nodes,
+                start_frame, end_frame,
+                delete_static_anim_curves)
+
     if len(mkr_nodes) > 0:
         maya.cmds.select(mkr_nodes, replace=True)
     return

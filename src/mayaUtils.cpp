fcbbce842b32752f5148d0b4492da62fcd8477ae
David Cattermole <cattermole91@gmail.com>
2021-12-29 18:53:22 +1100

    Move functions from header to source file; from mayaUtils.h to .cpp.
    Issue #114.

--- src/mayaUtils.cpp
+++ src/mayaUtils.cpp
@@ -23,16 +23,24 @@
 
 // Maya
 #include <maya/MStatus.h>
+#include <maya/MPoint.h>
+#include <maya/MVector.h>
+#include <maya/MString.h>
+#include <maya/MStringArray.h>
 #include <maya/MObject.h>
 #include <maya/MObjectArray.h>
+#include <maya/MDagPath.h>
+#include <maya/MPlug.h>
+#include <maya/MSelectionList.h>
+#include <maya/MFnDependencyNode.h>
 #include <maya/MPxNode.h>
 #include <maya/MStreamUtils.h>
 
 MStatus
 MMNodeInitUtils::attributeAffectsMulti(
         const MObjectArray &inputAttrs,
-        const MObjectArray &outputAttrs)
-{
+        const MObjectArray &outputAttrs
+) {
     MStatus status = MS::kSuccess;
     for (unsigned int i = 0; i < inputAttrs.length(); i++) {
         for (unsigned int j = 0; j < outputAttrs.length(); j++) {
@@ -49,5 +57,181 @@ MMNodeInitUtils::attributeAffectsMulti(
             }
         }
     }
+    return status;
+}
+
+
+MStatus getAsSelectionList(
+    MStringArray nodeNames,
+    MSelectionList &selList
+) {
+    MStatus status;
+    for (unsigned int i = 0; i < nodeNames.length(); ++i) {
+        status = selList.add(nodeNames[i]);
+        CHECK_MSTATUS_AND_RETURN_IT(status);
+    }
+    if (selList.length() != nodeNames.length()) {
+        status = MStatus::kFailure;
+        status.perror("getAsSelectionList failed");
+    }
+    return status;
+}
+
+
+MStatus getAsSelectionList(
+    MString nodeName,
+    MSelectionList &selList
+) {
+    MStringArray nodeNames;
+    nodeNames.append(nodeName);
+    return getAsSelectionList(nodeNames, selList);
+}
+
+
+MStatus nodeExistsAndIsType(
+    MString nodeName,
+    MFn::Type nodeType
+) {
+    MStatus status;
+    MSelectionList selList;
+    selList.clear();
+
+    status = getAsSelectionList(nodeName, selList);
+    if (status != MS::kSuccess) {
+        ERR("Node does not exist; " << nodeName);
+    }
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    MObject nodeObj;
+    status = selList.getDependNode(0, nodeObj);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    if (nodeObj.apiType() != nodeType) {
+        ERR("Node type is not correct; node=" << nodeName << " type=" << nodeType);
+        status = MS::kFailure;
+        status.perror("Node Type is not correct");
+        return status;
+    }
+    return status;
+}
+
+
+MStatus getAsObject(
+    MString nodeName,
+    MObject &object
+) {
+    MStatus status;
+    MSelectionList selList;
+    status = getAsSelectionList(nodeName, selList);
+    CHECK_MSTATUS(status);
+    if (selList.length() == 1) {
+        status = selList.getDependNode(0, object);
+        CHECK_MSTATUS(status);
+    }
+    return status;
+}
+
+
+MStatus getAsDagPath(
+    MString nodeName,
+    MDagPath &nodeDagPath
+) {
+    MStatus status;
+    MSelectionList selList;
+    status = getAsSelectionList(nodeName, selList);
+    CHECK_MSTATUS(status);
+    if (selList.length() == 1) {
+        status = selList.getDagPath(0, nodeDagPath);
+        CHECK_MSTATUS(status);
+    }
+    return status;
+}
+
+
+bool hasAttrName(
+    MFnDependencyNode &dependFn,
+    MString attrName
+) {
+    MPlug plug = dependFn.findPlug(attrName, true);
+    return plug.isNull();
+}
+
+
+ObjectType computeObjectType(
+    MObject node_obj,
+    MDagPath nodeDagPath
+) {
+    bool hasLocatorShape = false;
+    bool hasCameraShape = false;
+    bool hasImagePlaneShape = false;
+    MFn::Type node_tid = nodeDagPath.apiType();
+    std::vector<MFn::Type> shape_tids;
+    unsigned int num_children = nodeDagPath.childCount();
+    for (unsigned int i = 0; i < num_children; ++i) {
+        MObject child_obj = nodeDagPath.child(i);
+        nodeDagPath.push(child_obj);
+        MFn::Type shape_tid = nodeDagPath.apiType();
+        shape_tids.push_back(shape_tid);
+        if (shape_tid == MFn::kLocator) {
+            hasLocatorShape = true;
+        } else if (shape_tid == MFn::kCamera) {
+            hasCameraShape = true;
+        } else if (shape_tid == MFn::kImagePlane) {
+            hasImagePlaneShape = true;
+        }
+    }
+
+    MFnDependencyNode dependFn(node_obj);
+    unsigned int objectType = OBJECT_TYPE_UNKNOWN;
+    bool hasAttrEnable = hasAttrName(dependFn, MString("enable"));
+    bool hasAttrWeight = hasAttrName(dependFn, MString("weight"));
+    bool hasAttrBundle = hasAttrName(dependFn, MString("bundle"));
+    bool hasAttrSolverList = hasAttrName(dependFn, "solver_list");
+    if (node_tid == MFn::kTransform
+        && hasLocatorShape
+        && hasAttrEnable
+        && hasAttrWeight
+        && hasAttrBundle) {
+        objectType = OBJECT_TYPE_MARKER;
+    } else if (node_tid == MFn::kTransform
+               && hasLocatorShape) {
+        objectType = OBJECT_TYPE_BUNDLE;
+    } else if (node_tid == MFn::kTransform
+               && hasCameraShape) {
+        objectType = OBJECT_TYPE_CAMERA;
+    } else if (node_tid == MFn::kCamera) {
+        objectType = OBJECT_TYPE_CAMERA;
+    } else if (node_tid == MFn::kTransform
+               && hasImagePlaneShape) {
+        objectType = OBJECT_TYPE_IMAGE_PLANE;
+    } else if (hasImagePlaneShape) {
+        objectType = OBJECT_TYPE_IMAGE_PLANE;
+    } else if (node_tid == MFn::kPluginDependNode) {
+        // TODO: Check specifically for 'mmMarkerGroupTransform' node type.
+        objectType = OBJECT_TYPE_MARKER_GROUP;
+    } else if (node_tid == MFn::kSet && hasAttrSolverList) {
+        objectType = OBJECT_TYPE_COLLECTION;
+    }
+
+    return static_cast<ObjectType>(objectType);
+}
+
+
+MStatus constructAttrAffectsName(
+    MString attrName,
+    MString attrUuidStr,
+    MString &outAttrName
+) {
+    MStatus status = MStatus::kSuccess;
+
+    const MString attrNamePrefix = "node_";
+    const MString attrNameSuffix = "_attr_";
+
+    status = attrName.substitute(".", "_");
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+
+    outAttrName = attrNamePrefix + attrUuidStr + attrNameSuffix + attrName;
+    status = outAttrName.substitute("-", "_");
+    CHECK_MSTATUS_AND_RETURN_IT(status);
     return status;
 }
--- src/mayaUtils.h
+++ src/mayaUtils.h
@@ -31,8 +31,6 @@
 
 // Maya
 #include <maya/MStatus.h>
-#include <maya/MPoint.h>
-#include <maya/MVector.h>
 #include <maya/MString.h>
 #include <maya/MStringArray.h>
 #include <maya/MObject.h>
@@ -41,7 +39,6 @@
 #include <maya/MPlug.h>
 #include <maya/MSelectionList.h>
 #include <maya/MFnDependencyNode.h>
-#include <maya/MPxNode.h>
 
 
 #define OBJECT_TYPE_UNKNOWN      (0)
@@ -67,172 +64,47 @@ enum class ObjectType
 };
 
 
-static inline
-MStatus getAsSelectionList(MStringArray nodeNames, MSelectionList &selList) {
-    MStatus status;
-    for (unsigned int i = 0; i < nodeNames.length(); ++i) {
-        status = selList.add(nodeNames[i]);
-        CHECK_MSTATUS_AND_RETURN_IT(status);
-    }
-    if (selList.length() != nodeNames.length()) {
-        status = MStatus::kFailure;
-        status.perror("getAsSelectionList failed");
-    }
-    return status;
-}
+MStatus getAsSelectionList(
+    MStringArray nodeNames,
+    MSelectionList &selList);
 
 
-static inline
-MStatus getAsSelectionList(MString nodeName, MSelectionList &selList) {
-    MStringArray nodeNames;
-    nodeNames.append(nodeName);
-    return getAsSelectionList(nodeNames, selList);
-}
+MStatus getAsSelectionList(
+    MString nodeName,
+    MSelectionList &selList);
 
 
-static inline
-MStatus nodeExistsAndIsType(MString nodeName, MFn::Type nodeType) {
-    MStatus status;
-    MSelectionList selList;
-    selList.clear();
-
-    status = getAsSelectionList(nodeName, selList);
-    if (status != MS::kSuccess) {
-        ERR("Node does not exist; " << nodeName);
-    }
-    CHECK_MSTATUS_AND_RETURN_IT(status);
-
-    MObject nodeObj;
-    status = selList.getDependNode(0, nodeObj);
-    CHECK_MSTATUS_AND_RETURN_IT(status);
-
-    if (nodeObj.apiType() != nodeType) {
-        ERR("Node type is not correct; node=" << nodeName << " type=" << nodeType);
-        status = MS::kFailure;
-        status.perror("Node Type is not correct");
-        return status;
-    }
-    return status;
-}
+MStatus nodeExistsAndIsType(
+    MString nodeName,
+    MFn::Type nodeType);
 
 
-static inline
-MStatus getAsObject(MString nodeName, MObject &object) {
-    MStatus status;
-    MSelectionList selList;
-    status = getAsSelectionList(nodeName, selList);
-    CHECK_MSTATUS(status);
-    if (selList.length() == 1) {
-        status = selList.getDependNode(0, object);
-        CHECK_MSTATUS(status);
-    }
-    return status;
-}
+MStatus getAsObject(
+    MString nodeName,
+    MObject &object);
 
 
-static inline
-MStatus getAsDagPath(MString nodeName, MDagPath &nodeDagPath) {
-    MStatus status;
-    MSelectionList selList;
-    status = getAsSelectionList(nodeName, selList);
-    CHECK_MSTATUS(status);
-    if (selList.length() == 1) {
-        status = selList.getDagPath(0, nodeDagPath);
-        CHECK_MSTATUS(status);
-    }
-    return status;
-}
+MStatus getAsDagPath(
+    MString nodeName,
+    MDagPath &nodeDagPath);
 
-
-inline
-bool hasAttrName(MFnDependencyNode &dependFn, MString attrName) {
-    MPlug plug = dependFn.findPlug(attrName, true);
-    return plug.isNull();
-}
+bool hasAttrName(
+    MFnDependencyNode &dependFn,
+    MString attrName);
 
 
 // Analogous to the Python function "mmSolver.api.get_object_type()"
-inline
 ObjectType computeObjectType(
     MObject node_obj,
-    MDagPath nodeDagPath
-) {
-    bool hasLocatorShape = false;
-    bool hasCameraShape = false;
-    bool hasImagePlaneShape = false;
-    MFn::Type node_tid = nodeDagPath.apiType();
-    std::vector<MFn::Type> shape_tids;
-    unsigned int num_children = nodeDagPath.childCount();
-    for (unsigned int i = 0; i < num_children; ++i) {
-        MObject child_obj = nodeDagPath.child(i);
-        nodeDagPath.push(child_obj);
-        MFn::Type shape_tid = nodeDagPath.apiType();
-        shape_tids.push_back(shape_tid);
-        if (shape_tid == MFn::kLocator) {
-            hasLocatorShape = true;
-        } else if (shape_tid == MFn::kCamera) {
-            hasCameraShape = true;
-        } else if (shape_tid == MFn::kImagePlane) {
-            hasImagePlaneShape = true;
-        }
-    }
-
-    MFnDependencyNode dependFn(node_obj);
-    unsigned int objectType = OBJECT_TYPE_UNKNOWN;
-    bool hasAttrEnable = hasAttrName(dependFn, MString("enable"));
-    bool hasAttrWeight = hasAttrName(dependFn, MString("weight"));
-    bool hasAttrBundle = hasAttrName(dependFn, MString("bundle"));
-    bool hasAttrSolverList = hasAttrName(dependFn, "solver_list");
-    if (node_tid == MFn::kTransform
-        && hasLocatorShape
-        && hasAttrEnable
-        && hasAttrWeight
-        && hasAttrBundle) {
-        objectType = OBJECT_TYPE_MARKER;
-    } else if (node_tid == MFn::kTransform
-               && hasLocatorShape) {
-        objectType = OBJECT_TYPE_BUNDLE;
-    } else if (node_tid == MFn::kTransform
-               && hasCameraShape) {
-        objectType = OBJECT_TYPE_CAMERA;
-    } else if (node_tid == MFn::kCamera) {
-        objectType = OBJECT_TYPE_CAMERA;
-    } else if (node_tid == MFn::kTransform
-               && hasImagePlaneShape) {
-        objectType = OBJECT_TYPE_IMAGE_PLANE;
-    } else if (hasImagePlaneShape) {
-        objectType = OBJECT_TYPE_IMAGE_PLANE;
-    } else if (node_tid == MFn::kPluginDependNode) {
-        // TODO: Check specifically for 'mmMarkerGroupTransform' node type.
-        objectType = OBJECT_TYPE_MARKER_GROUP;
-    } else if (node_tid == MFn::kSet && hasAttrSolverList) {
-        objectType = OBJECT_TYPE_COLLECTION;
-    }
-
-    return static_cast<ObjectType>(objectType);
-}
+    MDagPath nodeDagPath);
 
 
 // Generate attribute name used to set and look up 'attribute affects'
 // on nodes.
-static inline
-MStatus constructAttrAffectsName(MString attrName,
-                                 MString attrUuidStr,
-                                 MString &outAttrName){
-    MStatus status = MStatus::kSuccess;
-
-    const MString attrNamePrefix = "node_";
-    const MString attrNameSuffix = "_attr_";
-
-    status = attrName.substitute(".", "_");
-    CHECK_MSTATUS_AND_RETURN_IT(status);
-
-    outAttrName = attrNamePrefix + attrUuidStr + attrNameSuffix + attrName;
-    status = outAttrName.substitute("-", "_");
-    CHECK_MSTATUS_AND_RETURN_IT(status);
-    return status;
-}
-
+MStatus constructAttrAffectsName(
+    MString attrName,
+    MString attrUuidStr,
+    MString &outAttrName);
 
 // Static attributes to help with Maya Node initialization.
 class MMNodeInitUtils {

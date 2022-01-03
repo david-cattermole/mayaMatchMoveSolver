a445d01d39d61d7fc1f367757e1d703221f46bed
ktonegawa <kazuma.tonegawa@gmail.com>
2022-01-02 22:57:56 -0800

    initial commit; simply porting/adding the ml_convertRotationOrder.py and ml_utilities.py files and adding it to the menu

---
+++ python/mmSolver/tools/mltools/__init__.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2019, 2021 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+These are direct ports for Morgan Loomis's Animation tools so that they 
+can be utilized within mmSolver.
+
+Morgan Loomis's tools are under the MIT License making them compatible 
+with mmSolver's GNU Lesser General Public License.
+
+The original files can be found here:
+http://morganloomis.com/
+"""

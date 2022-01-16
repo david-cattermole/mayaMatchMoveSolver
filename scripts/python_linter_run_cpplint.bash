2e97ff94e45790e2a0e98647bb39d64689f8d576
David Cattermole <cattermole91@gmail.com>
2022-01-17 01:14:45 +1100

    Add scripts to run cpplint in Travis CI

---
+++ scripts/python_linter_run_cpplint.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+#
+# Runs PyLint for the project.
+
+# The -e flag causes the script to exit as soon as one command returns
+# a non-zero exit code.
+set -ev
+
+PROJECT_ROOT=`pwd`
+
+python -m pylint --recursive "${PROJECT_ROOT}/src" "${PROJECT_ROOT}/include"
+
+cd ${PROJECT_ROOT}

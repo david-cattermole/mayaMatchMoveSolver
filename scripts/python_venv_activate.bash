3b19d04457703991e4c08f211a16c91dbe837feb
David Cattermole <cattermole91@gmail.com>
2022-01-20 05:22:46 +1100

    Add Python virtual envionment build scripts for Linux.
    Ensure the files are set with a 'x' executable permission bit
    on the files.

---
+++ scripts/python_venv_activate.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+#
+# Activates (and sets up) the Python development environment.
+#
+# Warning: This file expects the variables "PYTHON_VIRTUAL_ENV_DIR_NAME"
+# and "PYTHON_EXE" to be defined in the calling environment, if this is
+# not done, undefined behaviour will happen (likely an error).
+
+
+# The -e flag causes the script to exit as soon as one command returns
+# a non-zero exit code.
+set -ev
+
+PROJECT_ROOT=`pwd`
+
+# Clear the currently generated Python virtual environment before
+# running the build process (including Python commands).
+#
+# The generation of a Python Virtual Environment slow to run and should
+# be reused if possible; therefore leave this off for normal usage.
+FRESH_PYTHON_VIRTUAL_ENV=0
+
+# Activate script file name.
+PYTHON_VIRTUAL_ENV_ACTIVATE_SCRIPT=${PYTHON_VIRTUAL_ENV_DIR_NAME}/bin/activate
+
+# Delete any existing Python virtual environment, if it exists.
+if [ ${FRESH_PYTHON_VIRTUAL_ENV} -eq 1 ]; then
+    mkdir %PYTHON_VIRTUAL_ENV_DIR_NAME%
+    cd "${PYTHON_VIRTUAL_ENV_DIR_NAME}"
+    rm -f -R *
+    cd "${PROJECT_ROOT}"
+fi
+
+# Ensure Python Virtual Environment is setup.
+if [ ! -f "${PYTHON_VIRTUAL_ENV_ACTIVATE_SCRIPT}" ]; then
+    echo "Setting up Python Virtual Environment"
+    ${PYTHON_EXE} -m venv ${PYTHON_VIRTUAL_ENV_DIR_NAME}
+fi
+
+# Activate!
+echo "Activating Python Virtual Environment ${PYTHON_VIRTUAL_ENV_DIR_NAME}"
+source "${PYTHON_VIRTUAL_ENV_ACTIVATE_SCRIPT}"
+
+# Install requirements
+${PYTHON_EXE} -m pip install --upgrade pip -r "${PROJECT_ROOT}/requirements-dev.txt"
+
+cd ${PROJECT_ROOT}
---
+++ scripts/python_venv_activate_maya2016.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2022 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+#
+# Activates the Python development environment for Maya 2018.
+
+# The -e flag causes the script to exit as soon as one command returns
+# a non-zero exit code.
+set -ev
+
+PROJECT_ROOT=`pwd`
+
+MAYA_VERSION=2016
+
+# Python executable - edit this to point to an explicit python executable file.
+PYTHON_EXE=python
+
+PYTHON_VIRTUAL_ENV_DIR_NAME="python_venv_linux_maya${MAYA_VERSION}"
+source "${PROJECT_ROOT}/scripts/python_venv_activate.bash"

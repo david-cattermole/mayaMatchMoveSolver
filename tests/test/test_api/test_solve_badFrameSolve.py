53425373f7c8e657461048dda34b855479539293
David Cattermole <cattermole91@gmail.com>
2022-03-27 21:43:00 +1100

    'test_solve_badFrameSolve.py' runs on multiple solvers/scene graphs.
    This helps to ensure MM Scene Graph is accurate.

--- tests/test/test_api/test_solve_badFrameSolve.py
+++ tests/test/test_api/test_solve_badFrameSolve.py
@@ -83,11 +83,19 @@ class TestSolveBadPerFrameSolve(test_api_utils.APITestCase):
         self.checkSolveResults(solres_list)
         return
 
-    def test_badPerFrameSolve_solverstandard(self):
+    def do_solve(self, solver_name, solver_type_index, scene_graph_mode):
         """
         The same test as 'test_badPerFrameSolve', but using the SolverStandard class.
         """
+        if self.haveSolverType(name=solver_name) is False:
+            msg = '%r solver is not available!' % solver_name
+            raise unittest.SkipTest(msg)
+        scene_graph_name = mmapi.SCENE_GRAPH_MODE_NAME_LIST[scene_graph_mode]
+        scene_graph_label = mmapi.SCENE_GRAPH_MODE_LABEL_LIST[scene_graph_mode]
+        print('Scene Graph:', scene_graph_label)
+
         s = time.time()
+
         # Open the Maya file
         file_name = 'mmSolverBasicSolveA_badSolve01.ma'
         path = self.get_data_path('scenes', file_name)
@@ -117,6 +125,8 @@ class TestSolveBadPerFrameSolve(test_api_utils.APITestCase):
         sol.set_use_single_frame(False)
         sol.set_global_solve(False)
         sol.set_only_root_frames(False)
+        sol.set_solver_type(solver_type_index)
+        sol.set_scene_graph_mode(scene_graph_mode)
         sol_list = [sol]
         print('Solver:', sol)
         e = time.time()
@@ -142,6 +152,24 @@ class TestSolveBadPerFrameSolve(test_api_utils.APITestCase):
 
         self.checkSolveResults(solres_list)
         return
+
+    def test_ceres_maya_dag(self):
+        self.do_solve('ceres', mmapi.SOLVER_TYPE_CERES, mmapi.SCENE_GRAPH_MODE_MAYA_DAG)
+
+    def test_ceres_mmscenegraph(self):
+        self.do_solve('ceres', mmapi.SOLVER_TYPE_CERES, mmapi.SCENE_GRAPH_MODE_MM_SCENE_GRAPH)
+
+    def test_cminpack_lmdif_maya_dag(self):
+        self.do_solve('cminpack_lmdif', mmapi.SOLVER_TYPE_CMINPACK_LMDIF, mmapi.SCENE_GRAPH_MODE_MAYA_DAG)
+
+    def test_cminpack_lmdif_mmscenegraph(self):
+        self.do_solve('cminpack_lmdif', mmapi.SOLVER_TYPE_CMINPACK_LMDIF, mmapi.SCENE_GRAPH_MODE_MM_SCENE_GRAPH)
+
+    def test_cminpack_lmder_maya_dag(self):
+        self.do_solve('cminpack_lmder', mmapi.SOLVER_TYPE_CMINPACK_LMDER, mmapi.SCENE_GRAPH_MODE_MAYA_DAG)
+
+    def test_cminpack_lmder_mmscenegraph(self):
+        self.do_solve('cminpack_lmder', mmapi.SOLVER_TYPE_CMINPACK_LMDER, mmapi.SCENE_GRAPH_MODE_MM_SCENE_GRAPH)
 
 
 if __name__ == '__main__':

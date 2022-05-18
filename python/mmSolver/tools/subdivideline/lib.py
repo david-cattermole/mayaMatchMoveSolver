b534f18d984629f50740004f17d1eb3b6801e223
David Cattermole <cattermole91@gmail.com>
2022-05-18 22:16:21 +1000

    Subdivide individual line segments to an arbitrary level.
    This allows adding more detail in areas that are needed without
    adding line segments where they are not needed.
    
    The tool works on the current Marker or Line selection. If a line is
    selected the line is uniformly subdivided. If markers are selected,
    the line segments around the markers are subdivided.

--- python/mmSolver/tools/subdivideline/lib.py
+++ python/mmSolver/tools/subdivideline/lib.py
@@ -23,101 +23,108 @@ import maya.cmds
 import mmSolver.logger
 import mmSolver.api as mmapi
 
+DEFAULT_MARKER_NAME = 'marker1'
 LOG = mmSolver.logger.get_logger()
 
 
-def subdivide_line(line):
+def _create_new_line_markers(mkr_grp):
+    mkr_name_a = mmapi.get_new_marker_name(DEFAULT_MARKER_NAME)
+    mkr_a = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_a)
+    mkr_name_b = mmapi.get_new_marker_name(DEFAULT_MARKER_NAME)
+    mkr_b = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_b)
+
+    mkr_node_a = mkr_a.get_node()
+    mkr_node_b = mkr_b.get_node()
+    maya.cmds.setAttr(mkr_node_a + '.tx', -0.25)
+    maya.cmds.setAttr(mkr_node_b + '.tx', 0.25)
+    maya.cmds.setAttr(mkr_node_a + '.ty', -0.15)
+    maya.cmds.setAttr(mkr_node_b + '.ty', 0.15)
+    return mkr_a, mkr_b
+
+
+def _create_new_marker_pair(mkr_grp, line_tfm, mkr_start, mkr_end):
+    mkr_new_name = mmapi.get_new_marker_name(DEFAULT_MARKER_NAME)
+    mkr_new = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_new_name)
+
+    mkr_node_a = mkr_start.get_node()
+    mkr_node_b = mkr_new.get_node()
+    mkr_node_c = mkr_end.get_node()
+
+    tx_b = maya.cmds.getAttr(mkr_node_a + '.tx')
+    tx_b += maya.cmds.getAttr(mkr_node_c + '.tx')
+    tx_b = tx_b * 0.5
+
+    ty_b = maya.cmds.getAttr(mkr_node_a + '.ty')
+    ty_b += maya.cmds.getAttr(mkr_node_c + '.ty')
+    ty_b = ty_b * 0.5
+
+    maya.cmds.setAttr(mkr_node_b + '.tx', tx_b)
+    maya.cmds.setAttr(mkr_node_b + '.ty', ty_b)
+    maya.cmds.parent(mkr_node_b, line_tfm, relative=True)
+    return mkr_new
+
+
+def _create_n_new_markers(mkr_grp, line_tfm, mkr_list, current_all_mkr_list):
+    new_mkr_list = []
+    new_all_mkr_node_list = [x.get_node() for x in current_all_mkr_list]
+    new_all_mkr_list = list(current_all_mkr_list)
+
+    mkr_list_start = mkr_list
+    mkr_list_end = mkr_list[1:]
+    mkr_pairs = list(zip(mkr_list_start, mkr_list_end))
+    for mkr_start, mkr_end in mkr_pairs:
+        mkr_new = _create_new_marker_pair(
+            mkr_grp,
+            line_tfm,
+            mkr_start,
+            mkr_end)
+
+        mkr_index = new_all_mkr_node_list.index(mkr_start.get_node())
+        mkr_next_index = mkr_index + 1
+        new_all_mkr_list.insert(mkr_next_index, mkr_new)
+        new_all_mkr_node_list.insert(mkr_next_index, mkr_new.get_node())
+
+        new_mkr_list.append(mkr_new)
+
+    return new_all_mkr_list, new_mkr_list
+
+
+def subdivide_line(line, mkr_list):
     """
-    Set the lock value for the given Marker nodes.
+    Subdivides the line, with the given marker list.
+
+    The line segments between each marker in 'mkr_list' will be
+    subdivided.
+
+    :param line: The line to subdivide.
+    :type line: mmapi.Line
+
+    :param mkr_list: List of marker to subdivide between. Each pair of
+        markers represents a 'line segment' that will have a new Marker
+        added.
+    :type mkr_list: [mmapi.Marker, ..]
+
+    :rtype: ([mmapi.Marker, ..], [mmapi.Marker, ..])
+    :returns: Two lists of markers, first list is all the markers that
+        are part of the new line, and the second list are the newly
+        created markers.
     """
-    mkr_list = line.get_marker_list()
     num_mkrs = len(mkr_list)
 
-    sel = []
+    all_mkr_list = []
     new_mkr_list = []
     line_tfm = line.get_node()
     mkr_grp = line.get_marker_group()
     if num_mkrs == 0:
-        mkr_name_a = mmapi.get_new_marker_name('marker1')
-        mkr_a = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_a)
-        mkr_name_b = mmapi.get_new_marker_name('marker1')
-        mkr_b = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_b)
-        new_mkr_list = [mkr_a, mkr_b]
-
-        mkr_node_a = mkr_a.get_node()
-        mkr_node_b = mkr_b.get_node()
-        maya.cmds.setAttr(mkr_node_a + '.tx', -0.25)
-        maya.cmds.setAttr(mkr_node_b + '.tx', 0.25)
-        maya.cmds.setAttr(mkr_node_a + '.ty', -0.15)
-        maya.cmds.setAttr(mkr_node_b + '.ty', 0.15)
-        sel = [mkr_a, mkr_b]
-
-    elif num_mkrs == 2:
-        mkr_name_b = mmapi.get_new_marker_name('marker1')
-        mkr_a = mkr_list[0]
-        mkr_b = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_b)
-        mkr_c = mkr_list[1]
-        new_mkr_list = [mkr_a, mkr_b, mkr_c]
-
-        mkr_node_a = mkr_a.get_node()
-        mkr_node_b = mkr_b.get_node()
-        mkr_node_c = mkr_c.get_node()
-
-        tx_b = maya.cmds.getAttr(mkr_node_a + '.tx')
-        tx_b += maya.cmds.getAttr(mkr_node_c + '.tx')
-        tx_b = tx_b * 0.5
-
-        ty_b = maya.cmds.getAttr(mkr_node_a + '.ty')
-        ty_b += maya.cmds.getAttr(mkr_node_c + '.ty')
-        ty_b = ty_b * 0.5
-
-        maya.cmds.setAttr(mkr_node_b + '.tx', tx_b)
-        maya.cmds.setAttr(mkr_node_b + '.ty', ty_b)
-        maya.cmds.parent(mkr_node_b, line_tfm, relative=True)
-        sel = [mkr_b]
-
-    elif num_mkrs == 3:
-        mkr_a = mkr_list[0]
-        mkr_name_b = mmapi.get_new_marker_name('marker1')
-        mkr_b = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_b)
-        mkr_c = mkr_list[1]
-        mkr_name_d = mmapi.get_new_marker_name('marker1')
-        mkr_d = mmapi.Marker().create_node(mkr_grp=mkr_grp, name=mkr_name_d)
-        mkr_e = mkr_list[2]
-        new_mkr_list = [mkr_a, mkr_b, mkr_c, mkr_d, mkr_e]
-
-        mkr_node_a = mkr_a.get_node()
-        mkr_node_b = mkr_b.get_node()
-        mkr_node_c = mkr_c.get_node()
-        mkr_node_d = mkr_d.get_node()
-        mkr_node_e = mkr_e.get_node()
-
-        tx_b = maya.cmds.getAttr(mkr_node_a + '.tx')
-        tx_b += maya.cmds.getAttr(mkr_node_c + '.tx')
-        tx_b = tx_b * 0.5
-
-        ty_b = maya.cmds.getAttr(mkr_node_a + '.ty')
-        ty_b += maya.cmds.getAttr(mkr_node_c + '.ty')
-        ty_b = ty_b * 0.5
-
-        tx_d = maya.cmds.getAttr(mkr_node_c + '.tx')
-        tx_d += maya.cmds.getAttr(mkr_node_e + '.tx')
-        tx_d = tx_d * 0.5
-
-        ty_d = maya.cmds.getAttr(mkr_node_c + '.ty')
-        ty_d += maya.cmds.getAttr(mkr_node_e + '.ty')
-        ty_d = ty_d * 0.5
-
-        maya.cmds.setAttr(mkr_node_b + '.tx', tx_b)
-        maya.cmds.setAttr(mkr_node_b + '.ty', ty_b)
-        maya.cmds.setAttr(mkr_node_d + '.tx', tx_d)
-        maya.cmds.setAttr(mkr_node_d + '.ty', ty_d)
-        maya.cmds.parent(mkr_node_b, line_tfm, relative=True)
-        maya.cmds.parent(mkr_node_d, line_tfm, relative=True)
-        sel = [mkr_b, mkr_d]
-
+        mkr_a, mkr_b = _create_new_line_markers(mkr_grp)
+        all_mkr_list = [mkr_a, mkr_b]
+        new_mkr_list = all_mkr_list
     else:
-        LOG.warn('Not supported yet.')
+        current_all_mkr_list = line.get_marker_list()
+        all_mkr_list, new_mkr_list = _create_n_new_markers(
+            mkr_grp,
+            line_tfm,
+            mkr_list,
+            current_all_mkr_list)
 
-    line.set_marker_list(new_mkr_list)
-    return sel
+    return all_mkr_list, new_mkr_list
--- python/mmSolver/tools/subdivideline/tool.py
+++ python/mmSolver/tools/subdivideline/tool.py
@@ -15,9 +15,18 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
 #
+"""Subdivide the number of markers in selected line.
+
+This allows adding more detail in areas that are needed without
+adding line segments where they are not needed.
+
+The tool works on the current Marker or Line selection. If a line is
+selected the line is uniformly subdivided. If markers are selected,
+the line segments around the markers are subdivided.
+
 """
-Subdivide the number of markers in selected line.
-"""
+
+import collections
 
 import maya.cmds
 import mmSolver.logger
@@ -25,6 +34,36 @@ import mmSolver.api as mmapi
 import mmSolver.tools.subdivideline.lib as lib
 
 LOG = mmSolver.logger.get_logger()
+
+
+def _expand_markers_to_line_segments(line, mkr_node_list):
+    mkr_node_list = [mmapi.Marker(node=x).get_node()
+                     for x in mkr_node_list]
+
+    all_mkr_list = line.get_marker_list()
+    all_mkr_node_list = [x.get_node() for x in all_mkr_list]
+    all_mkr_index_list = [all_mkr_node_list.index(x)
+                          for x in all_mkr_node_list]
+
+    mkr_node_to_index = {}
+    for mkr_node, mkr_index in zip(all_mkr_node_list, all_mkr_index_list):
+        mkr_node_to_index[mkr_node] = mkr_index
+
+    mkr_index_set = set()
+    for mkr_node in mkr_node_list:
+        mkr_index = mkr_node_to_index.get(mkr_node)
+        if mkr_index is None:
+            continue
+        mkr_index_min = max(0, mkr_index - 1)
+        mkr_index_max = min(mkr_index + 1, len(all_mkr_list) - 1)
+        mkr_index_set.add(mkr_index_min)
+        mkr_index_set.add(mkr_index)
+        mkr_index_set.add(mkr_index_max)
+
+    mkr_list = [all_mkr_list[mkr_index]
+                for mkr_index in sorted(mkr_index_set)]
+
+    return mkr_list
 
 
 def main():
@@ -38,12 +77,14 @@ def main():
         LOG.warning("Please select lines or line markers to subdivide.")
         return
 
+    line_to_mkr_list_map = collections.defaultdict(list)
     line_nodes = []
     if len(selected_line_nodes) > 0:
         line_nodes = selected_line_nodes
     elif len(selected_marker_nodes) > 0:
         for node in selected_marker_nodes:
             line_node = mmapi.get_line_above_node(node)
+            line_to_mkr_list_map[line_node].append(node)
             line_nodes.append(line_node)
     line_nodes = sorted(set(line_nodes))
     line_nodes = [x for x in line_nodes if x]
@@ -51,10 +92,27 @@ def main():
     new_mkr_list = []
     for line_node in line_nodes:
         line = mmapi.Line(node=line_node)
-        new_mkr_list += lib.subdivide_line(line)
+
+        mkr_list = line.get_marker_list()
+        mkr_node_list = line_to_mkr_list_map[line_node]
+
+        no_markers = len(mkr_node_list) == 0
+        all_markers = len(mkr_node_list) == len(mkr_list)
+        if not no_markers or not all_markers:
+            mkr_list = _expand_markers_to_line_segments(line, mkr_node_list)
+
+        all_mkrs, new_mkrs = lib.subdivide_line(line, mkr_list)
+        new_mkr_list += new_mkrs
+
+        # The full list of markers for the line, keeping the
+        # non-subdivided markers as they are. The order of the list is
+        # important, as this is the order of the individual line
+        # segments.
+        line.set_marker_list(all_mkrs)
 
     new_mkr_nodes = [x.get_node() for x in new_mkr_list]
     if len(new_mkr_nodes) > 0:
+        new_mkr_nodes = list(sorted(set(new_mkr_nodes)))
         maya.cmds.select(new_mkr_nodes, replace=True)
     else:
         maya.cmds.select(selection, replace=True)

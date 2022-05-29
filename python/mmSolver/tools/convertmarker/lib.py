3a7e55ff6a6848b0655d41b3c6a36b1578d1424e
David Cattermole <cattermole91@gmail.com>
2022-05-30 02:29:51 +1000

    Add Python3 compatibility to 'mmSolver.tools.convertmarker.lib'

--- python/mmSolver/tools/convertmarker/lib.py
+++ python/mmSolver/tools/convertmarker/lib.py
@@ -19,6 +19,10 @@
 The 'Convert to Marker' library functions.
 """
 
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import itertools
 
 import maya.cmds

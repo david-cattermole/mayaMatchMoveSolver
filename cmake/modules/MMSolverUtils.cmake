fe27c1f18b4a41413233d471767fdb66eb7993aa
David Cattermole <cattermole91@gmail.com>
2022-07-03 15:06:23 +1000

    Remove build define of 'GFLAGS_DLL_DECL'.
    We do not use gflags anymore, so this is not needed.

--- cmake/modules/MMSolverUtils.cmake
+++ cmake/modules/MMSolverUtils.cmake
@@ -66,8 +66,6 @@ function(set_global_maya_plugin_compile_options)
     # doesn't support it.
     add_compile_definitions(GLOG_NO_ABBREVIATED_SEVERITIES)
 
-    add_compile_definitions(GFLAGS_DLL_DECL)
-
     # Ceres running on Windows can trigger a MSVC warning:
     #
     # "Compiler Warning (level 3) C4996: Your code uses a function,

51ab473cc22c75e47220afdca2ad366fbd729beb
David Cattermole <cattermole91@gmail.com>
2022-10-16 21:20:04 +1100

    Add "Set Object Colour" tool
    The tool does what it says.
    
    Select some objects and run the tool, a UI will be displayed to choose
    the colour. You can use a "mini" UI, or the full UI (the option box).
    
    In Maya 2023+ an alpha value is also available to make objects
    transparent.

---
+++ python/mmSolver/tools/setobjectcolour/lib.py
@@ -0,0 +1,111 @@
+# Copyright (C) 2022 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Getting and setting colours for objects.
+"""
+
+import maya.cmds
+
+import mmSolver.logger
+import mmSolver.utils.time as time_utils
+import mmSolver.utils.node as node_utils
+import mmSolver.api as mmapi
+
+LOG = mmSolver.logger.get_logger()
+
+
+def get_first_node_colour(tfm_nodes):
+    rgba = [0.5, 0.5, 0.5, 1.0]
+    for tfm_node in tfm_nodes:
+        object_type = mmapi.get_object_type(tfm_node)
+        if object_type == mmapi.OBJECT_TYPE_MARKER:
+            mkr = mmapi.Marker(node=tfm_node)
+            rgba = mkr.get_colour_rgba()
+            return rgba
+        elif object_type == mmapi.OBJECT_TYPE_BUNDLE:
+            bnd = mmapi.Bundle(node=tfm_node)
+            rgba = bnd.get_colour_rgba()
+            return rgba
+        elif object_type == mmapi.OBJECT_TYPE_LINE:
+            line = mmapi.Line(node=tfm_node)
+            rgba = line.get_colour_rgba()
+            return rgba
+        else:
+            node_types = ('surfaceShape', 'locator')
+            shp_nodes = (
+                maya.cmds.ls(tfm_node, long=True, dag=True, type=node_types) or []
+            )
+            for shp_node in shp_nodes:
+                rgba = node_utils.get_node_draw_override_colour_rgba(shp_node)
+                if rgba:
+                    return rgba
+    return rgba
+
+
+def reset_nodes_colour(tfm_nodes):
+    for tfm_node in tfm_nodes:
+        is_ref = node_utils.node_is_referenced(tfm_node)
+        if is_ref is True:
+            continue
+
+        object_type = mmapi.get_object_type(tfm_node)
+        if object_type == mmapi.OBJECT_TYPE_MARKER:
+            mkr = mmapi.Marker(node=tfm_node)
+            mkr.set_colour_rgba(None)
+        elif object_type == mmapi.OBJECT_TYPE_BUNDLE:
+            bnd = mmapi.Bundle(node=tfm_node)
+            bnd.set_colour_rgba(None)
+        elif object_type == mmapi.OBJECT_TYPE_LINE:
+            line = mmapi.Line(node=tfm_node)
+            line.set_colour_rgba(None)
+        else:
+            node_types = ('surfaceShape', 'locator', 'camera')
+            shp_nodes = (
+                maya.cmds.ls(tfm_node, long=True, dag=True, type=node_types) or []
+            )
+            for shp_node in shp_nodes:
+                node_utils.set_node_draw_override_enabled(shp_node, False)
+    return
+
+
+def set_nodes_colour(tfm_nodes, rgba):
+    assert len(rgba) >= 4
+    for tfm_node in tfm_nodes:
+        is_ref = node_utils.node_is_referenced(tfm_node)
+        if is_ref is True:
+            continue
+
+        object_type = mmapi.get_object_type(tfm_node)
+        if object_type == mmapi.OBJECT_TYPE_MARKER:
+            mkr = mmapi.Marker(node=tfm_node)
+            mkr.set_colour_rgba(rgba)
+        elif object_type == mmapi.OBJECT_TYPE_BUNDLE:
+            bnd = mmapi.Bundle(node=tfm_node)
+            bnd.set_colour_rgba(rgba)
+        elif object_type == mmapi.OBJECT_TYPE_LINE:
+            line = mmapi.Line(node=tfm_node)
+            line.set_colour_rgba(rgba)
+        else:
+            node_types = ('surfaceShape', 'locator', 'camera')
+            shp_nodes = (
+                maya.cmds.ls(tfm_node, long=True, dag=True, type=node_types) or []
+            )
+            for shp_node in shp_nodes:
+                node_utils.set_node_draw_override_enabled(shp_node, True)
+                node_utils.set_node_draw_override_colour_rgba(shp_node, rgba)
+    return
---
+++ python/mmSolver/tools/setobjectcolour/tool.py
@@ -0,0 +1,94 @@
+# Copyright (C) 2022 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+#
+"""
+Set the colour of selected objects.
+"""
+
+import mmSolver.ui.qtpyutils as qtpyutils
+
+qtpyutils.override_binding_order()
+
+import mmSolver.ui.Qt.QtCore as QtCore
+import mmSolver.ui.Qt.QtGui as QtGui
+import mmSolver.ui.Qt.QtWidgets as QtWidgets
+
+import maya.cmds
+
+import mmSolver.logger
+import mmSolver.utils.node as node_utils
+import mmSolver.tools.setobjectcolour.lib as lib
+import mmSolver.api as mmapi
+
+LOG = mmSolver.logger.get_logger()
+
+
+def reset_colour():
+    sel = maya.cmds.ls(sl=True, long=True) or []
+
+    tfm_nodes = maya.cmds.ls(sl=True, long=True, type='transform') or []
+    if len(tfm_nodes) == 0:
+        LOG.warn('Please select shape nodes.')
+        return
+
+    lib.reset_nodes_colour(tfm_nodes)
+
+    if len(sel) > 0:
+        maya.cmds.select(sel, replace=True)
+    return
+
+
+def open_window(mini_ui=None):
+    if mini_ui is None:
+        mini_ui = False
+
+    sel = maya.cmds.ls(sl=True, long=True) or []
+
+    tfm_nodes = maya.cmds.ls(sl=True, long=True, type='transform') or []
+    if len(tfm_nodes) == 0:
+        LOG.warn('Please select shape nodes.')
+        return
+
+    # Initial colour to start the 'colorEditor' with.
+    rgba = lib.get_first_node_colour(tfm_nodes)
+    rgb = (rgba[0], rgba[1], rgba[2])
+    alpha = rgba[3]
+
+    kwargs = {'mini': False}
+    if mini_ui is True:
+        cursor_point = QtGui.QCursor().pos()
+        window_pos = (cursor_point.x() - 50, cursor_point.y() - 50)
+        kwargs = {'mini': True, 'position': window_pos}
+
+    maya.cmds.colorEditor(rgbValue=rgb, alpha=alpha, **kwargs)
+    if not maya.cmds.colorEditor(query=True, result=True):
+        LOG.warn('Color editor cancelled.')
+        return
+
+    rgb = maya.cmds.colorEditor(query=True, rgbValue=True)
+    alpha = maya.cmds.colorEditor(query=True, alpha=True)
+
+    rgba = (rgb[0], rgb[1], rgb[2], alpha)
+    lib.set_nodes_colour(tfm_nodes, rgba)
+
+    if len(sel) > 0:
+        maya.cmds.select(sel, replace=True)
+    return
+
+
+def open_mini_window():
+    open_window(mini_ui=True)

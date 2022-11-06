2fad7459e1621b1c270447660d183cab2c74bb49
David Cattermole <cattermole91@gmail.com>
2022-11-06 17:30:31 +1100

    Fix Maya imports for Camera Solver

--- python/mmSolver/_api/solvercamera.py
+++ python/mmSolver/_api/solvercamera.py
@@ -23,6 +23,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import maya.cmds
+
 import mmSolver.logger
 
 import mmSolver.utils.python_compat as pycompat

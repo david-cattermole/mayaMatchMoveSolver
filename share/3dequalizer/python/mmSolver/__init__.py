8a20d72d03cff13a2d0141bb42b64ceecf1a4248
David Cattermole <cattermole91@gmail.com>
2022-12-11 22:06:12 +1100

    Remove unneeded duplicate Python script files.

--- share/3dequalizer/python/mmSolver/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = "David Cattermole and others (see AUTHORS.txt file)"
-__copyright__ = "Copyright (C) 2018-2022, David Cattermole, Anil Reddy, Kazuma Tonegawa, Patcha Saheb Binginapalli"
-__license__ = "GNU LGPL v3"

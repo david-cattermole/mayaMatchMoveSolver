1d5812350f628987dffcae5d2a864f7b62e7d08f
David Cattermole <cattermole91@gmail.com>
2023-02-05 22:54:25 +1100

    Solver Standard - Simplify setting 'frame solve mode'.

--- python/mmSolver/_api/solverstandardutils.py
+++ python/mmSolver/_api/solverstandardutils.py
@@ -416,8 +416,7 @@ def _compile_multi_inbetween_frames(
             sol.set_use_stiffness(use_smooth_stiff)
             sol.set_solver_type(solver_type)
             sol.set_scene_graph_mode(scene_graph_mode)
-            frame_solve_mode = const.FRAME_SOLVE_MODE_PER_FRAME
-            sol.set_frame_solve_mode(frame_solve_mode)
+            sol.set_frame_solve_mode(const.FRAME_SOLVE_MODE_PER_FRAME)
             sol.set_remove_unused_markers(remove_unused_objects)
             sol.set_remove_unused_attributes(remove_unused_objects)
             sol.set_precomputed_data(precomputed_data)

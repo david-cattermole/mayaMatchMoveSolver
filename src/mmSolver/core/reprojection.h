526d446c06416c6fbbd5398ea0156e58554e1772
David Cattermole <cattermole91@gmail.com>
2023-02-12 15:27:32 +1100

    Add lens distortion support for reprojection
    This also fixes issues of the output coordinates of markers (and other
    screen-space coordinates) not correctly being scaled when the input
    image width/height is not the exact same aspect ratio as the film back
    width/height aspect ratio.

--- src/mmSolver/core/reprojection.h
+++ src/mmSolver/core/reprojection.h
@@ -23,10 +23,30 @@
 #ifndef MM_SOLVER_CORE_REPROJECTION_H
 #define MM_SOLVER_CORE_REPROJECTION_H
 
+// STL
+#include <memory>
+
 // Maya
 #include <maya/MMatrix.h>
 #include <maya/MStatus.h>
 
+// MM Solver
+#include "mmSolver/lens/lens_model.h"
+
+enum class ReprojectionDistortMode : short {
+    // No change at all.
+    kNone = 0,
+
+    // Assumes the point is distorted and will undistort the point.
+    kUndistort = 1,
+
+    // Assumes the point is undistorted, and will recover the
+    // distorted point.
+    kRedistort = 2,
+};
+
+// Standard reprojection - no special handling of lens distortion in
+// this function.
 MStatus reprojection(
     const MMatrix tfmMatrix, const MMatrix camMatrix,
 
@@ -37,10 +57,51 @@ MStatus reprojection(
     const double nearClipPlane, const double farClipPlane,
     const double cameraScale,
 
-    // Image
+    // Image Resolution - so we can give values as pixels
     const double imageWidth, const double imageHeight,
 
     // Manipulation
+    const MMatrix applyMatrix, const bool overrideScreenX,
+    const bool overrideScreenY, const bool overrideScreenZ,
+    const double screenX, const double screenY, const double screenZ,
+    const double depthScale,
+
+    // Outputs
+    double &outCoordX, double &outCoordY, double &outNormCoordX,
+    double &outNormCoordY, double &outMarkerCoordX, double &outMarkerCoordY,
+    double &outMarkerCoord, double &outPixelX, double &outPixelY,
+    bool &outInsideFrustum, double &outPointX, double &outPointY,
+    double &outPointZ, double &outWorldPointX, double &outWorldPointY,
+    double &outWorldPointZ, MMatrix &outMatrix, MMatrix &outWorldMatrix,
+    MMatrix &outCameraProjectionMatrix,
+    MMatrix &outInverseCameraProjectionMatrix,
+    MMatrix &outWorldCameraProjectionMatrix,
+    MMatrix &outWorldInverseCameraProjectionMatrix, double &outHorizontalPan,
+    double &outVerticalPan);
+
+// Reprojection with extra lens distortion parameters, so that the
+// reprojection can undistort/redistort the reprojected point.
+MStatus reprojection(
+    const MMatrix tfmMatrix, const MMatrix camMatrix,
+
+    // Camera
+    const double focalLength, const double horizontalFilmAperture,
+    const double verticalFilmAperture, const double horizontalFilmOffset,
+    const double verticalFilmOffset, const short filmFit,
+    const double nearClipPlane, const double farClipPlane,
+    const double cameraScale,
+
+    // Image Resolution - so we can give values as pixels
+    const double imageWidth, const double imageHeight,
+
+    // Lens Distortion
+    const ReprojectionDistortMode distortMode,
+    std::shared_ptr<LensModel> lensModel,
+
+    // Manipulation
+    //
+    // These are applied *before* the point is adjusted by the lens
+    // distortion.
     const MMatrix applyMatrix, const bool overrideScreenX,
     const bool overrideScreenY, const bool overrideScreenZ,
     const double screenX, const double screenY, const double screenZ,

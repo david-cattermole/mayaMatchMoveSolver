aaab34d2e6b05549810d57cfa9281d3134cc9932
David Cattermole <cattermole91@gmail.com>
2023-04-08 22:26:31 +1000

    Add ruff linting script with auto-fix enabled.

---
+++ scripts/python_linter_run_ruff_fix.bat
@@ -0,0 +1,31 @@
+@ECHO OFF
+SETLOCAL
+::
+:: Copyright (C) 2021 David Cattermole.
+::
+:: This file is part of mmSolver.
+::
+:: mmSolver is free software: you can redistribute it and/or modify it
+:: under the terms of the GNU Lesser General Public License as
+:: published by the Free Software Foundation, either version 3 of the
+:: License, or (at your option) any later version.
+::
+:: mmSolver is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU Lesser General Public License for more details.
+::
+:: You should have received a copy of the GNU Lesser General Public License
+:: along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+:: ---------------------------------------------------------------------
+::
+:: Runs Ruff for the project, and auto-fixes problems.
+::
+:: Ruff - An extremely fast Python linter, written in Rust.
+:: - https://github.com/charliermarsh/ruff
+:: - https://beta.ruff.rs/docs/
+:: - https://pypi.org/project/ruff/
+
+SET PROJECT_ROOT=%CD%
+
+ruff check --fix "%PROJECT_ROOT%"

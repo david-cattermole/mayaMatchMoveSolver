ef0100703ef52aa90c227ce0fbb433526a6061ea
David Cattermole <cattermole91@gmail.com>
2023-10-27 23:21:01 +1100

    Marker - Convert frame range to list

--- python/mmSolver/_api/marker.py
+++ python/mmSolver/_api/marker.py
@@ -829,8 +829,7 @@ class Marker(object):
         anim_curve_fn = self.get_deviation_anim_curve_fn()
 
         if anim_curve_fn is None:
-            enable_times = range(frame_range_start, frame_range_end + 1)
-            enable_times = list(enable_times)
+            enable_times = list(range(frame_range_start, frame_range_end + 1))
         else:
             num_keys = anim_curve_fn.numKeys()
             enable_times = [None] * num_keys
@@ -838,8 +837,7 @@ class Marker(object):
                 mtime = anim_curve_fn.time(i)
                 enable_times[i] = int(mtime.value())
             if num_keys == 0:
-                enable_times = range(frame_range_start, frame_range_end + 1)
-                enable_times = list(enable_times)
+                enable_times = list(range(frame_range_start, frame_range_end + 1))
 
         start_frame = int(min(enable_times))
         end_frame = int(max(enable_times))

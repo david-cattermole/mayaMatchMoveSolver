a5d6b5d1e1d1392c246ef2c4ad026188cf7f38f1
David Cattermole <cattermole91@gmail.com>
2023-10-27 23:23:21 +1100

    Fix 'mmSolver.utils.time.get_frame_range()' function
    The wrong frame range will be returned given the frame_range_mode.

--- python/mmSolver/utils/time.py
+++ python/mmSolver/utils/time.py
@@ -124,9 +124,9 @@ def get_frame_range(frame_range_mode, start_frame=None, end_frame=None):
     assert frame_range_mode in const.FRAME_RANGE_MODE_VALUES
 
     if frame_range_mode == const.FRAME_RANGE_MODE_TIMELINE_INNER_VALUE:
-        start_frame, end_frame = get_maya_timeline_range_outer()
-    elif frame_range_mode == const.FRAME_RANGE_MODE_TIMELINE_OUTER_VALUE:
         start_frame, end_frame = get_maya_timeline_range_inner()
+    elif frame_range_mode == const.FRAME_RANGE_MODE_TIMELINE_OUTER_VALUE:
+        start_frame, end_frame = get_maya_timeline_range_outer()
     elif frame_range_mode == const.FRAME_RANGE_MODE_CUSTOM_VALUE:
         assert start_frame is not None
         assert end_frame is not None

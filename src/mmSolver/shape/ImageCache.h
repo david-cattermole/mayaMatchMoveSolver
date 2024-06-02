72b61cfa63510661a8613ace9faa4887590321a3
David Cattermole <cattermole91@gmail.com>
2024-06-02 22:57:35 +1000

    Add GPU/CPU capacity and eviction to ImageCache.
    This allows allocating CPU and GPU memory, and automatic eviction
    (deallocation) of cached GPU/CPU memory, to keep a spesified number of
    images in the cache.
    
    The capacity is currently based on the number of images, not the amount
    of memory - this will be changed in the future.
    
    Tests show that the number of GPU images stored can be quite low, and we
    can upload to the GPU from the CPU cache fairly fast (50 fps).
    
    The next steps would be to pre-allocate the anticipated GPU images that
    will be loaded in the future and allocate them from a secondary thread
    (or concurrently) to ensure smooth playback. This would allow a small
    number of GPU images backed by a much larger number of cached CPU
    images.
    
    GitHub Issue #252.

--- src/mmSolver/shape/ImageCache.h
+++ src/mmSolver/shape/ImageCache.h
@@ -23,6 +23,8 @@
 #define MM_IMAGE_CACHE_H
 
 // STL
+#include <algorithm>
+#include <list>
 #include <string>
 #include <unordered_map>
 
@@ -101,6 +103,46 @@ struct CacheImagePixelData {
 
     ~CacheImagePixelData() = default;
 
+    bool allocate_pixels(const uint32_t width, const uint32_t height,
+                         const uint8_t num_channels,
+                         const CachePixelDataType pixel_data_type) {
+        m_width = width;
+        m_height = height;
+        m_num_channels = num_channels;
+        m_pixel_data_type = pixel_data_type;
+
+        const size_t pixel_data_byte_count = CacheImagePixelData::byte_count();
+
+        if (pixel_data_byte_count == 0) {
+            MMSOLVER_MAYA_ERR("mmsolver::CacheImagePixelData:allocate_pixels: "
+                              << "Invalid image size for allocating pixel data!"
+                              << " width=" << m_width << " height=" << m_height
+                              << " num_channels=" << m_num_channels
+                              << " pixel_data_type="
+                              << static_cast<int>(m_pixel_data_type));
+            return false;
+        }
+
+        bool ok = false;
+        void *data = std::malloc(pixel_data_byte_count);
+        if (data) {
+            m_pixel_data = data;
+            ok = true;
+        } else {
+            ok = false;
+            MMSOLVER_MAYA_ERR(
+                "mmsolver::CacheImagePixelData:allocate_pixels: "
+                << "Could not allocate pixel data!"
+                << " requested=" << pixel_data_byte_count << "B"
+                << " requested="
+                << (static_cast<float>(pixel_data_byte_count) * 1e-6) << "MB");
+        }
+
+        return ok;
+    }
+
+    void deallocate_pixels() { std::free(m_pixel_data); }
+
     bool is_valid() const {
         return (m_pixel_data != nullptr) && (m_width != 0) && (m_height != 0) &&
                (m_num_channels != 0) &&
@@ -113,6 +155,12 @@ struct CacheImagePixelData {
     uint8_t num_channels() const { return m_num_channels; }
     CachePixelDataType pixel_data_type() const { return m_pixel_data_type; }
 
+    size_t byte_count() const {
+        const uint8_t bytes_per_channel =
+            convert_pixel_data_type_to_bytes_per_channel(m_pixel_data_type);
+        return m_width * m_height * m_num_channels * bytes_per_channel;
+    }
+
 private:
     void *m_pixel_data;
     uint32_t m_width;
@@ -124,7 +172,13 @@ private:
 // The ImageCache, used to load and cache images into GPU, CPU and
 // Disk.
 //
-// Singleton design pattern for C++11
+// Least-Recently-Used (LRU) Cache using standard C++11 data
+// structures.
+//
+// Inspired by:
+// https://web.archive.org/web/20161203001546/http://timday.bitbucket.org/lru.html
+//
+// Singleton design pattern for C++11:
 // https://stackoverflow.com/a/1008289
 //
 // TODO: Use 4 layers of reading textures.
@@ -147,25 +201,26 @@ private:
 // then placed into a queue to be saved into the disk cache. The queue
 // of images are processed off the main thread, so that the
 // interactive session does not slow down.
-
-// TODO: Read image (and insert into cache).
-//
-// TODO: Set/Get CPU memory allowed.
-//
-// TODO: Set/Get GPU memory allowed.
-//
-// TODO: Set/Get Disk space allowed.
-//
-// TODO: Set/Get Disk cache location. Used to find disk-cached
-// files. This should be a directory on a very fast disk.
 //
 // TODO: Support converting to 8-bit LDR image pixels before
 // storing. For example converting to sRGB colour space.
+//
 struct ImageCache {
-    using CPUCacheKey = std::string;
     using GPUCacheKey = std::string;
-    using CPUMap = std::unordered_map<CPUCacheKey, CacheImagePixelData>;
-    using GPUMap = std::unordered_map<GPUCacheKey, MTexture *>;
+    using GPUCacheValue = MTexture *;
+    using GPUKeyList = std::list<GPUCacheKey>;
+    using GPUKeyListIt = GPUKeyList::iterator;
+    using GPUMap =
+        std::unordered_map<GPUCacheKey, std::pair<GPUKeyListIt, GPUCacheValue>>;
+    using GPUMapIt = GPUMap::iterator;
+
+    using CPUCacheKey = std::string;
+    using CPUCacheValue = CacheImagePixelData;
+    using CPUKeyList = std::list<CPUCacheKey>;
+    using CPUKeyListIt = CPUKeyList::iterator;
+    using CPUMap =
+        std::unordered_map<CPUCacheKey, std::pair<CPUKeyListIt, CPUCacheValue>>;
+    using CPUMapIt = CPUMap::iterator;
 
 public:
     static ImageCache &getInstance() {
@@ -176,41 +231,119 @@ public:
 
 private:
     // Constructor. The {} brackets are needed here.
-    ImageCache() {}
+    ImageCache() : m_cpu_capacity(1), m_gpu_capacity(1) {}
+
+    template <typename KeyList, typename KeyListIt>
+    void move_iterator_to_back_of_key_list(KeyList &key_list,
+                                           KeyListIt key_list_iterator) {
+        // The back of the list is the "most recently used" key.
+        key_list.splice(key_list.end(), key_list, key_list_iterator);
+    }
 
 public:
-    // Insert pixels into image CPU cache.
+    // TODO: Set/Get GPU memory allowed.
+    // TODO: Set/Get CPU memory allowed.
+    size_t get_gpu_capacity() const {
+        const bool verbose = false;
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::get_cpu_capacity: "
+                          << "m_cpu_capacity=" << m_cpu_capacity);
+
+        return m_gpu_capacity;
+    }
+    size_t get_cpu_capacity() const {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::get_gpu_capacity: "
+                          << "m_gpu_capacity=" << m_gpu_capacity);
+
+        return m_cpu_capacity;
+    }
+
+    void set_gpu_capacity(const size_t capacity) {
+        const bool verbose = false;
+
+        // TODO: Evict the contents of the cache, if the current
+        // number of cached items exceeds the new capacity size.
+        m_gpu_capacity = capacity;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::set_gpu_capacity: "
+                          << "m_gpu_capacity=" << m_gpu_capacity);
+    }
+
+    void set_cpu_capacity(const size_t capacity) {
+        const bool verbose = false;
+
+        // TODO: Evict the contents of the cache, if the current
+        // number of cached items exceeds the new capacity size.
+        m_cpu_capacity = capacity;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::set_cpu_capacity: "
+                          << "m_cpu_capacity=" << m_cpu_capacity);
+    }
+
+    // TODO: Set/Get Disk space allowed.
+    //
+    // TODO: Set/Get Disk cache location. Used to find disk-cached
+    // files. This should be a directory on a very fast disk.
+
+    // Insert pixels into CPU cache.
     //
     // If the key is already in the image cache, the previous value is
     // erased.
+    //
+    // Returns true/false, if the the data was inserted or not.
     bool cpu_insert(const CPUCacheKey &key,
-                    const CacheImagePixelData &image_pixel_data) {
-        auto search = m_cpu_cache_map.find(key);
+                    const CPUCacheValue &image_pixel_data) {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::cpu_insert: "
+                          << "key=" << key);
+
+        const CPUMapIt search = m_cpu_cache_map.find(key);
 
         const bool found = search != m_cpu_cache_map.end();
         if (found) {
             ImageCache::cpu_erase(key);
         }
 
-        const bool ok = m_cpu_cache_map.insert({key, image_pixel_data}).second;
+        // 'key' is the most-recently-used key.
+        CPUKeyListIt key_iterator =
+            m_cpu_cache_key_list.insert(m_cpu_cache_key_list.end(), key);
+
+        const auto pair = m_cpu_cache_map.insert(std::make_pair(
+            key, std::make_pair(key_iterator, image_pixel_data)));
+
+        const auto inserted_key_iterator = pair.first;
+        const bool ok = pair.second;
+        assert(ok == true);
         return ok;
     }
 
-    // Insert
-    MTexture *gpu_insert(MHWRender::MTextureManager *texture_manager,
-                         const GPUCacheKey &key,
-                         const CacheImagePixelData &image_pixel_data) {
+    // Insert and upload some pixels to the GPU and cache the result.
+    //
+    // Returns the GPUCacheValue inserted into the GPU cache.
+    GPUCacheValue gpu_insert(MHWRender::MTextureManager *texture_manager,
+                             const GPUCacheKey &key,
+                             const CPUCacheValue &image_pixel_data) {
         assert(texture_manager != nullptr);
         assert(image_pixel_data.is_valid());
         const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::gpu_insert: "
+                          << "key=" << key);
 
         // No need for MIP-maps.
         const bool generate_mip_maps = false;
 
         MTexture *texture = nullptr;
-        auto search = m_gpu_cache_map.find(key);
+        const GPUMapIt search = m_gpu_cache_map.find(key);
         const bool found = search != m_gpu_cache_map.end();
         if (!found) {
+            // If we are at capacity, make room for new entry.
+            if (m_gpu_cache_map.size() >= m_gpu_capacity) {
+                ImageCache::gpu_evict_one(texture_manager);
+            }
+
             const uint32_t width = image_pixel_data.width();
             const uint32_t height = image_pixel_data.height();
             const uint8_t number_of_channels = image_pixel_data.num_channels();
@@ -267,8 +400,21 @@ public:
                 return false;
             }
 
+            // Make 'key' the most-recently-used key.
+            GPUKeyListIt key_iterator =
+                m_gpu_cache_key_list.insert(m_gpu_cache_key_list.end(), key);
+
+            // Create the key-value entry, linked to the usage record.
+            const auto pair = m_gpu_cache_map.insert(
+                std::make_pair(key, std::make_pair(key_iterator, texture)));
+
+            const auto inserted_key_iterator = pair.first;
+            const bool ok = pair.second;
+            assert(ok == true);
+
         } else {
-            texture = search->second;
+            GPUKeyListIt iterator = search->second.first;
+            texture = search->second.second;
             if (texture == nullptr) {
                 MMSOLVER_MAYA_ERR(
                     "mmsolver::ImageCache: gpu_insert: "
@@ -276,15 +422,14 @@ public:
                 return false;
             }
 
-            m_gpu_cache_map.erase(search);
+            move_iterator_to_back_of_key_list(m_gpu_cache_key_list, iterator);
 
             // TODO: If the 'texture_desc' is different than the
             // current texture, it cannot be updated, and must be
             // released and a new texture created instead.
-            // // ImageCache::gpu_erase(key);
 
-            // The default value of this argument is 0. This means to use
-            // the texture's width * the number of bytes per pixel.
+            // The default value of this argument is 0. This means to
+            // use the texture's "width * number of bytes per pixel".
             uint32_t rowPitch = 0;
 
             MHWRender::MTextureUpdateRegion *region = nullptr;
@@ -294,55 +439,152 @@ public:
             CHECK_MSTATUS(status);
         }
 
-        const bool ok = m_gpu_cache_map.insert({key, texture}).second;
-        assert(ok == true);
         return texture;
     }
 
-    // Find the key in the image GPU cache.
-    MTexture *gpu_find(const GPUCacheKey &key) {
-        auto search = m_gpu_cache_map.find(key);
+    // Find the key in the GPU cache.
+    //
+    // Returns the GPUCacheValue at the key, or nullptr.
+    GPUCacheValue gpu_find(const GPUCacheKey &key) {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::gpu_find: "
+                          << "key=" << key);
+
+        const GPUMapIt search = m_gpu_cache_map.find(key);
         const bool found = search != m_gpu_cache_map.end();
         if (found) {
-            return search->second;
+            GPUKeyListIt iterator = search->second.first;
+            GPUCacheValue value = search->second.second;
+            move_iterator_to_back_of_key_list(m_gpu_cache_key_list, iterator);
+            return value;
         }
         return nullptr;
     }
 
-    // Find the key in the image CPU cache.
-    CacheImagePixelData cpu_find(const CPUCacheKey &key) {
-        auto search = m_cpu_cache_map.find(key);
+    // Find the key in the CPU cache.
+    //
+    // Returns the CPUCacheValue at the key, or constructs a default
+    // value and returns it.
+    CPUCacheValue cpu_find(const CPUCacheKey &key) {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::cpu_find: "
+                          << "key=" << key);
+
+        const CPUMapIt search = m_cpu_cache_map.find(key);
         const bool found = search != m_cpu_cache_map.end();
         if (found) {
-            return search->second;
+            CPUKeyListIt iterator = search->second.first;
+            CPUCacheValue value = search->second.second;
+            move_iterator_to_back_of_key_list(m_cpu_cache_key_list, iterator);
+            return value;
         }
-        return CacheImagePixelData();
+        return CPUCacheValue();
+    }
+
+    // Evict the least recently used item from the GPU cache.
+    //
+    // Returns true/false, if an item was removed from the cache or
+    // not.
+    bool gpu_evict_one(MHWRender::MTextureManager *texture_manager) {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::gpu_evict_one: ");
+
+        assert(texture_manager != nullptr);
+        if (m_gpu_cache_key_list.empty()) {
+            return false;
+        }
+
+        const GPUCacheKey lru_key = m_gpu_cache_key_list.front();
+        const GPUMapIt lru_key_iterator = m_gpu_cache_map.find(lru_key);
+        assert(lru_key_iterator != m_gpu_cache_map.end());
+
+        GPUCacheValue texture = lru_key_iterator->second.second;
+        texture_manager->releaseTexture(texture);
+
+        m_gpu_cache_map.erase(lru_key_iterator);
+        m_gpu_cache_key_list.pop_front();
+        return true;
+    }
+
+    // Evict the least recently used item from the CPU cache.
+    //
+    // Returns true/false, if an item was removed from the cache or
+    // not.
+    bool cpu_evict_one() {
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::cpu_evict_one: ");
+
+        if (m_cpu_cache_key_list.empty()) {
+            return false;
+        }
+
+        const CPUCacheKey lru_key = m_cpu_cache_key_list.front();
+        const CPUMapIt lru_key_iterator = m_cpu_cache_map.find(lru_key);
+        assert(lru_key_iterator != m_cpu_cache_map.end());
+
+        CPUCacheValue value = lru_key_iterator->second.second;
+        value.deallocate_pixels();
+
+        m_cpu_cache_map.erase(lru_key_iterator);
+        m_cpu_cache_key_list.pop_front();
+        return true;
     }
 
     // Remove the key from the image GPU cache.
+    //
+    // NOTE: Due to the way the LRU cache works, this can be quite
+    // slow to to remove a specific key from the cache.
+    //
+    // Returns true/false, if the key was removed or not.
     bool gpu_erase(MHWRender::MTextureManager *texture_manager,
                    const GPUCacheKey &key) {
-        const auto search = m_gpu_cache_map.find(key);
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::gpu_erase: ");
+
+        const GPUMapIt search = m_gpu_cache_map.find(key);
         const bool found = search != m_gpu_cache_map.end();
         if (found) {
-            MTexture *texture = search->second;
+            GPUCacheValue value = search->second.second;
 
             assert(texture_manager != nullptr);
-            texture_manager->releaseTexture(texture);
+            texture_manager->releaseTexture(value);
+
             m_gpu_cache_map.erase(search);
+
+            // NOTE: This is a O(n) linear operation, and can be very
+            // slow since the list items is spread out in memory.
+            m_gpu_cache_key_list.remove(key);
         }
         return found;
     }
 
     // Remove the key from the image CPU cache.
+    //
+    // NOTE: Due to the way the LRU cache works, this can be quite
+    // slow to to remove a specific key from the cache.
+    //
+    // Returns true/false, if the key was removed or not.
     bool cpu_erase(const CPUCacheKey &key) {
-        auto search = m_cpu_cache_map.find(key);
+        const bool verbose = false;
+
+        MMSOLVER_MAYA_VRB("mmsolver::ImageCache::cpu_erase: ");
+
+        const CPUMapIt search = m_cpu_cache_map.find(key);
         const bool found = search != m_cpu_cache_map.end();
         if (found) {
-            CacheImagePixelData value = search->second;
-            // TODO: De-allocate the cache value. We must clear the
-            // existing memory before adding the new pixel data.
+            CPUCacheValue value = search->second.second;
+            value.deallocate_pixels();
+
             m_cpu_cache_map.erase(search);
+
+            // NOTE: This is a O(n) linear operation, and can be very
+            // slow since the list items is spread out in memory.
+            m_cpu_cache_key_list.remove(key);
         }
         return found;
     }
@@ -358,13 +600,36 @@ public:
     void operator=(ImageCache const &) = delete;
 
 private:
-    // TODO: Implement an LRU with standard C++.
+    // A Map of keys to values.
     //
-    // https://web.archive.org/web/20161203001546/http://timday.bitbucket.org/lru.html
-    // https://bitbucket.org/timday/lru_cache/src/master/include/lru_cache_using_std.h
+    // An unordered hash map is used to map file path strings to CPU
+    // Image Data and GPU texture resources.
     //
-    CPUMap m_cpu_cache_map;
+    // These maps also contain pointers into the 'key list' data
+    // structures, allowing us to map from the values to the 'key
+    // list's.
     GPUMap m_gpu_cache_map;
+    CPUMap m_cpu_cache_map;
+
+    // List of keys.
+    //
+    // A double-ended linked list (std::list) is used because we want
+    // to be able to push/pop and access the (front/back) ends of the
+    // list as fast as possible.
+    //
+    // Additionally, the linked list ensure that the pointers of each
+    // item in the list stays consistent over time, so that pointers
+    // into the memory can be used directly without fear that the
+    // underlying memory will be cleared.
+    //
+    // The 'front' of the list is the "least recently used" key.  The
+    // 'back' of the list is the "most recently used" key.
+    GPUKeyList m_gpu_cache_key_list;
+    CPUKeyList m_cpu_cache_key_list;
+
+    // Number of items allowed in the cache.
+    size_t m_gpu_capacity;
+    size_t m_cpu_capacity;
 
     // Pre-allocated empty string to be re-used inside the class, to
     // avoid reallocations.

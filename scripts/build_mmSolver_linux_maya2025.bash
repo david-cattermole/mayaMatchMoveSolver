52e8798f2cebf461817b0571382c08e39c23165d
David Cattermole <cattermole91@gmail.com>
2024-06-18 00:17:02 +1000

    Maya 2025 on Linux - Initial support added for building plug-in.
    Uses OpenColorIO 2.3.2 (upgraded from 2.2 in previous versions).
    
    This has not been tested well and has been confirmed to build, but there
    are currently symbols missing at runtime (which is a problem currently
    affecting all Linux builds with OpenColorIO).
    
    The location of Qt's Resource Compiler executable ("rcc") and UI
    Compiler ("uic") has been moved in the Linux install of Maya 2025 to the
    '${MAYA_LOCATION}/libexec' directory. The CMake script has been updated
    for this.

---
+++ scripts/build_mmSolver_linux_maya2025.bash
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2019, 2022, 2023, 2024 David Cattermole.
+#
+# This file is part of mmSolver.
+#
+# mmSolver is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# mmSolver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with mmSolver.  If not, see <https://www.gnu.org/licenses/>.
+# ---------------------------------------------------------------------
+#
+# Builds the Maya MatchMove Solver project.
+
+# Maya
+MAYA_VERSION=2025
+MAYA_LOCATION=/usr/autodesk/maya2025/
+
+# Executable names/paths used for build process.
+PYTHON_EXE=python3.9
+CMAKE_EXE=cmake3
+RUST_CARGO_EXE=cargo
+
+# OpenColorIO specific options.
+OPENCOLORIO_TARBALL_NAME="OpenColorIO-2.3.2.tar.gz"
+OPENCOLORIO_TARBALL_EXTRACTED_DIR_NAME="OpenColorIO-2.3.2"
+EXPAT_RELATIVE_CMAKE_DIR=lib64/cmake/expat-2.4.1/
+EXPAT_RELATIVE_LIB_PATH=lib64/libexpat.a
+# yaml-cpp 0.7.0
+YAML_RELATIVE_CMAKE_DIR=share/cmake/yaml-cpp
+YAML_RELATIVE_LIB_PATH=lib64/libyaml-cpp.a
+PYSTRING_RELATIVE_LIB_PATH=lib64/libpystring.a
+ZLIB_RELATIVE_LIB_PATH=lib/libz.a
+
+# Manually override OpenGL include headers, because CMake doesn't seem
+# to automatically find OpenGL headers on RockyLinux8 (which is used
+# in the Docker containers).
+OPENGL_INCLUDE_DIR=/usr/include/
+
+# Which version of the VFX platform are we "using"? (Maya doesn't
+# currently conform to the VFX Platform.)
+VFX_PLATFORM=2024
+
+# C++ Standard to use.
+CXX_STANDARD=14
+
+# The -e flag causes the script to exit as soon as one command returns
+# a non-zero exit code.
+set -ev
+
+CWD=`pwd`
+
+# These scripts assume 'RUST_CARGO_EXE' has been set to the Rust
+# 'cargo' executable.
+source "${CWD}/scripts/internal/build_openColorIO_linux.bash"
+source "${CWD}/scripts/internal/build_mmSolverLibs_linux.bash"
+source "${CWD}/scripts/internal/build_mmSolver_linux.bash"
+
+cd ${CWD}

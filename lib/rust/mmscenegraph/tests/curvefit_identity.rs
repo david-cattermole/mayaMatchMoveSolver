5b5bb022c37c530e44bd9418a01e3e7a7fc4e879
David Cattermole <cattermole91@gmail.com>
2024-11-02 12:31:40 +1100

    Curve Fit - Avoid warnings in tests
    GitHub issue #268.

--- lib/rust/mmscenegraph/tests/curvefit_identity.rs
+++ lib/rust/mmscenegraph/tests/curvefit_identity.rs
@@ -18,6 +18,10 @@
 // ====================================================================
 //
 
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
 mod common;
 
 use anyhow::Result;

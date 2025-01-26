e0e92cf73e572642fb39708dc5900231e45e1946
David Cattermole <cattermole91@gmail.com>
2025-01-26 12:54:57 +1100

    Add Solver Options User Preference
    This new option allows users to change to "developer" solver options, so
    they can change to the Ceres LMDER or LMDIF solvers for testing
    purposes.
    
    The default value will always be intended for regular users, trying to
    hide the underlying complexity from regular users.

--- python/mmSolver/tools/solver/widget/solver_standard_widget.py
+++ python/mmSolver/tools/solver/widget/solver_standard_widget.py
@@ -42,9 +42,18 @@ import mmSolver.tools.solver.widget.ui_solver_standard_widget as ui_solver_stand
 import mmSolver.tools.solver.widget.framerange_widget as framerange_widget
 import mmSolver.tools.solver.widget.rootframe_widget as rootframe_widget
 import mmSolver.tools.solver.constant as const
+import mmSolver.tools.userpreferences.constant as userprefs_const
+import mmSolver.tools.userpreferences.lib as userprefs_lib
 
 
 LOG = mmSolver.logger.get_logger()
+
+
+def get_user_prefs_solver_options_as_developer():
+    config = userprefs_lib.get_config()
+    key = userprefs_const.SOLVER_UI_SOLVER_OPTIONS_KEY
+    value = userprefs_lib.get_value(config, key)
+    return value == userprefs_const.SOLVER_UI_SOLVER_OPTIONS_DEVELOPER_VALUE
 
 
 def _populateWidgetsEnabled(widgets):
@@ -148,6 +157,10 @@ class SolverStandardWidget(QtWidgets.QWidget, ui_solver_standard_widget.Ui_Form)
         self.rootFrames_widget = StandardRootFrameWidget(self)
         self.rootFrames_layout.addWidget(self.rootFrames_widget)
 
+        developer_options = get_user_prefs_solver_options_as_developer()
+        scene_graph_mode_widget_visible = developer_options
+        solver_type_widget_visible = developer_options
+
         # Solver Type Combo Box.
         self.solverType_model = uimodels.StringDataListModel()
         self.solverType_model.setStringDataList(const.SOLVER_TYPE_LABEL_VALUE_LIST)
@@ -155,8 +168,8 @@ class SolverStandardWidget(QtWidgets.QWidget, ui_solver_standard_widget.Ui_Form)
         self.solverType_comboBox.currentIndexChanged.connect(
             self.solverTypeIndexChanged
         )
-        self.solverType_comboBox.setVisible(const.SOLVER_TYPE_WIDGET_VISIBLE)
-        self.solverType_label.setVisible(const.SOLVER_TYPE_WIDGET_VISIBLE)
+        self.solverType_comboBox.setVisible(solver_type_widget_visible)
+        self.solverType_label.setVisible(solver_type_widget_visible)
 
         # Scene Graph Mode Combo Box.
         self.sceneGraphMode_model = uimodels.StringDataListModel()
@@ -167,8 +180,8 @@ class SolverStandardWidget(QtWidgets.QWidget, ui_solver_standard_widget.Ui_Form)
         self.sceneGraphMode_comboBox.currentIndexChanged.connect(
             self.sceneGraphModeIndexChanged
         )
-        self.sceneGraphMode_comboBox.setVisible(const.SCENE_GRAPH_MODE_WIDGET_VISIBLE)
-        self.sceneGraphMode_label.setVisible(const.SCENE_GRAPH_MODE_WIDGET_VISIBLE)
+        self.sceneGraphMode_comboBox.setVisible(scene_graph_mode_widget_visible)
+        self.sceneGraphMode_label.setVisible(scene_graph_mode_widget_visible)
 
         self.globalSolve_checkBox.toggled.connect(self.globalSolveValueToggled)
         self.onlyRootFrames_checkBox.toggled.connect(self.onlyRootFramesValueToggled)

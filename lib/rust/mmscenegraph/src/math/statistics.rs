979612605b503e27269d3be985335589b14cb4a7
David Cattermole <cattermole91@gmail.com>
2026-02-20 19:23:49 +1100

    statistics.rs - Add TODO note for future

--- lib/rust/mmscenegraph/src/math/statistics.rs
+++ lib/rust/mmscenegraph/src/math/statistics.rs
@@ -25,6 +25,10 @@ use anyhow::Result;
 use std::cell::Cell;
 use thiserror::Error;
 
+// TODO: This file should not be in the mmscenegraph crate because
+// it's very reusuable and flexible. Other crates cannot use this
+// module because doing so would create a circular dependency.
+
 use crate::constant::Real;
 
 #[derive(Error, Debug)]

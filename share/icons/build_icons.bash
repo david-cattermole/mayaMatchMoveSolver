d424e053514c90cf8abe7bd2b3e51e72c74b37fb
David Cattermole <cattermole91@gmail.com>
2026-03-11 23:02:40 +1100

    build icons - Add working Bash script.
    To work on Linux.

--- share/icons/build_icons.bash
+++ share/icons/build_icons.bash
@@ -28,6 +28,93 @@ set -e
 
 THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# TODO: Write this script to match the Windows .bat equal.
+INKSCAPE_EXE="inkscape"
+
+OUTPUT_DIR="${THIS_DIR}"
 
 cd ${THIS_DIR}
+
+# Export node PNG icons at 20 x 20 resolution, for use in the Outliner.
+#
+# 20 x 20 is the resolution expected by Maya's Outliner.
+OUT_RESOLUTION=20
+for i in edit/node/*.svg; do
+    name=$(basename "${i}" .svg)
+    echo "-------------------------------------------------------------"
+    echo "Input file: ${i}"
+    echo "Output file: ${OUTPUT_DIR}/out_${name}.png"
+
+    ${INKSCAPE_EXE} \
+        --without-gui \
+        --export-area-page \
+        --export-width=${OUT_RESOLUTION} \
+        --export-height=${OUT_RESOLUTION} \
+        --file="${i}" \
+        --export-png="${OUTPUT_DIR}/out_${name}.png"
+done
+
+# Export node plain SVG icons.
+OUT_RESOLUTION=20
+for i in edit/node/*.svg; do
+    name=$(basename "${i}" .svg)
+    echo "-------------------------------------------------------------"
+    echo "Input file: ${i}"
+    echo "Output file: ${OUTPUT_DIR}/${name}.svg"
+
+    ${INKSCAPE_EXE} \
+        --without-gui \
+        --export-area-page \
+        --file="${i}" \
+        --export-plain-svg="${OUTPUT_DIR}/${name}.svg"
+done
+
+# Export shelf PNG icons at 32 x 32 resolution.
+OUT_RESOLUTION=32
+for i in edit/shelf/*.svg; do
+    name=$(basename "${i}" .svg)
+    echo "-------------------------------------------------------------"
+    echo "Input file: ${i}"
+    echo "Output file: ${OUTPUT_DIR}/${name}_${OUT_RESOLUTION}x${OUT_RESOLUTION}.png"
+
+    ${INKSCAPE_EXE} \
+        --without-gui \
+        --export-area-page \
+        --export-width=${OUT_RESOLUTION} \
+        --export-height=${OUT_RESOLUTION} \
+        --file="${i}" \
+        --export-png="${OUTPUT_DIR}/${name}.png"
+done
+
+# Export shelf PNG icons at 48 x 48 resolution (150% scale).
+OUT_RESOLUTION=48
+for i in edit/shelf/*.svg; do
+    name=$(basename "${i}" .svg)
+    echo "-------------------------------------------------------------"
+    echo "Input file: ${i}"
+    echo "Output file: ${OUTPUT_DIR}/${name}_150.png"
+
+    ${INKSCAPE_EXE} \
+        --without-gui \
+        --export-area-page \
+        --export-width=${OUT_RESOLUTION} \
+        --export-height=${OUT_RESOLUTION} \
+        --file="${i}" \
+        --export-png="${OUTPUT_DIR}/${name}_150.png"
+done
+
+# Export shelf PNG icons at 64 x 64 resolution (200% scale).
+OUT_RESOLUTION=64
+for i in edit/shelf/*.svg; do
+    name=$(basename "${i}" .svg)
+    echo "-------------------------------------------------------------"
+    echo "Input file: ${i}"
+    echo "Output file: ${OUTPUT_DIR}/${name}_200.png"
+
+    ${INKSCAPE_EXE} \
+        --without-gui \
+        --export-area-page \
+        --export-width=${OUT_RESOLUTION} \
+        --export-height=${OUT_RESOLUTION} \
+        --file="${i}" \
+        --export-png="${OUTPUT_DIR}/${name}_200.png"
+done
